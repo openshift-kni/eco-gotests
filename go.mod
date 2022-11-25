30a38f3fe7d1528d8e14a0e7d626fd18fac6a82e
Shereen Haj <shajmakh@redhat.com>
2022-11-25 22:29:40 +0200

    pkg: introduce pod package and bump dependencies (#8)
    * pkg: introduce pod package
    
    Add the new pkg/pod initially containing basic functions to work with pods like Create, Delete, ExecCommand and waiting for pod's status.
    
    Signed-off-by: shereenH <shajmakh@redhat.com>
    
    * vendor: update dependencies
    
    Signed-off-by: shereenH <shajmakh@redhat.com>
    
    Signed-off-by: shereenH <shajmakh@redhat.com>

--- go.mod
+++ go.mod
@@ -20,6 +20,7 @@ require (
 	k8s.io/apiextensions-apiserver v0.24.0
 	k8s.io/apimachinery v0.24.3
 	k8s.io/client-go v1.5.2
+	k8s.io/utils v0.0.0-20220210201930-3a6ce19ff2f9
 	sigs.k8s.io/controller-runtime v0.12.1
 )
 
@@ -68,6 +69,7 @@ require (
 	github.com/matttproud/golang_protobuf_extensions v1.0.2-0.20181231171920-c182affec369 // indirect
 	github.com/mitchellh/copystructure v1.2.0 // indirect
 	github.com/mitchellh/reflectwalk v1.0.2 // indirect
+	github.com/moby/spdystream v0.2.0 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
@@ -100,7 +102,6 @@ require (
 	k8s.io/component-base v0.24.3 // indirect
 	k8s.io/klog/v2 v2.60.1 // indirect
 	k8s.io/kube-openapi v0.0.0-20220328201542-3ee0da9b0b42 // indirect
-	k8s.io/utils v0.0.0-20220210201930-3a6ce19ff2f9 // indirect
 	sigs.k8s.io/json v0.0.0-20211208200746-9f7c6b3444d2 // indirect
 	sigs.k8s.io/structured-merge-diff/v4 v4.2.1 // indirect
 	sigs.k8s.io/yaml v1.3.0 // indirect
---
+++ pkg/pod/pod.go
@@ -0,0 +1,262 @@
+package pod
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"os"
+	"time"
+
+	v1 "k8s.io/api/core/v1"
+	k8serrors "k8s.io/apimachinery/pkg/api/errors"
+	metaV1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/wait"
+	"k8s.io/client-go/kubernetes/scheme"
+	"k8s.io/client-go/tools/remotecommand"
+	"k8s.io/utils/pointer"
+
+	"github.com/golang/glog"
+
+	"github.com/openshift-kni/eco-gotests/pkg/clients"
+)
+
+// Builder provides a struct for pod object from the cluster and a pod definition.
+type Builder struct {
+	// Pod definition, used to create pod object.
+	Definition *v1.Pod
+	// Created pod object.
+	Object *v1.Pod
+	// Used to store latest error message upon defining or mutating pod definition.
+	errorMsg string
+	// api client to interact with the cluster.
+	apiClient *clients.Settings
+}
+
+// NewBuilder creates new instance of Builder.
+func NewBuilder(name, nsName, image string, apiClient *clients.Settings) *Builder {
+	builder := &Builder{
+		apiClient:  apiClient,
+		Definition: getDefinition(name, nsName, image),
+	}
+
+	if name == "" {
+		builder.errorMsg = "pod's name is empty"
+	}
+
+	if nsName == "" {
+		builder.errorMsg = "namespace's name is empty"
+	}
+
+	if image == "" {
+		builder.errorMsg = "pod's image is empty"
+	}
+
+	return builder
+}
+
+// DefineOnNode adds node name on the pod's definition.
+func (builder *Builder) DefineOnNode(nodeName string) *Builder {
+	if builder.Definition == nil {
+		builder.errorMsg = "can not define pod on specific node because basic definition is empty"
+	}
+
+	if builder.Object != nil {
+		builder.errorMsg = fmt.Sprintf(
+			"can not redefine running pod. pod already running on node %s", builder.Object.Spec.NodeName)
+	}
+
+	if nodeName == "" {
+		builder.errorMsg = "can not define pod on empty node"
+	}
+
+	if builder.errorMsg == "" {
+		builder.Definition.Spec.NodeName = nodeName
+	}
+
+	return builder
+}
+
+// Create creates a pod according to the pod definition and stores the created object in the pod builder.
+func (builder *Builder) Create() (*Builder, error) {
+	if builder.errorMsg != "" {
+		return nil, fmt.Errorf(builder.errorMsg)
+	}
+
+	var err error
+	if !builder.Exists() {
+		builder.Object, err = builder.apiClient.Pods(builder.Definition.Namespace).Create(
+			context.TODO(), builder.Definition, metaV1.CreateOptions{})
+	}
+
+	return builder, err
+}
+
+// Delete deletes pod object and resets builder object.
+func (builder *Builder) Delete() (*Builder, error) {
+	if !builder.Exists() {
+		return builder, fmt.Errorf("pod cannot be deleted because it does not exist")
+	}
+
+	err := builder.apiClient.Pods(builder.Definition.Namespace).Delete(
+		context.TODO(), builder.Object.Name, metaV1.DeleteOptions{})
+
+	if err != nil {
+		return builder, fmt.Errorf("can not delete pod: %w", err)
+	}
+
+	builder.Object = nil
+
+	return builder, nil
+}
+
+// DeleteAndWait delete pod object and waits until deleted.
+func (builder *Builder) DeleteAndWait(timeout time.Duration) (*Builder, error) {
+	builder, err := builder.Delete()
+	if err != nil {
+		return builder, err
+	}
+
+	err = builder.WaitUntilDeleted(timeout)
+
+	if err != nil {
+		return builder, err
+	}
+
+	return builder, nil
+}
+
+// CreateAndWaitUntilRunning creates pod object and waits until pod is running.
+func (builder *Builder) CreateAndWaitUntilRunning(timeout time.Duration) (*Builder, error) {
+	builder, err := builder.Create()
+	if err != nil {
+		return builder, err
+	}
+
+	err = builder.WaitUntilRunning(timeout)
+
+	if err != nil {
+		return builder, err
+	}
+
+	return builder, nil
+}
+
+// WaitUntilRunning waits for timeout duration or until pod is running.
+func (builder *Builder) WaitUntilRunning(timeout time.Duration) error {
+	return builder.WaitUntilInStatus(v1.PodRunning, timeout)
+}
+
+// WaitUntilInStatus waits for timeout duration or until pod gets to a specific status.
+func (builder *Builder) WaitUntilInStatus(status v1.PodPhase, timeout time.Duration) error {
+	if builder.errorMsg != "" {
+		return fmt.Errorf(builder.errorMsg)
+	}
+
+	return wait.PollImmediate(time.Second, timeout, func() (bool, error) {
+		updatePod, err := builder.apiClient.Pods(builder.Object.Namespace).Get(
+			context.Background(), builder.Object.Name, metaV1.GetOptions{})
+		if err != nil {
+			return false, nil
+		}
+
+		return updatePod.Status.Phase == status, nil
+	})
+}
+
+// WaitUntilDeleted waits for timeout duration or until pod is deleted.
+func (builder *Builder) WaitUntilDeleted(timeout time.Duration) error {
+	err := wait.Poll(time.Second, timeout, func() (bool, error) {
+		_, err := builder.apiClient.Pods(builder.Definition.Namespace).Get(
+			context.Background(), builder.Definition.Name, metaV1.GetOptions{})
+		if err == nil {
+			glog.V(100).Infof("pod %s/%s still present", builder.Definition.Namespace, builder.Definition.Name)
+
+			return false, nil
+		}
+		if k8serrors.IsNotFound(err) {
+			glog.V(100).Infof("pod %s/%s is gone", builder.Definition.Namespace, builder.Definition.Name)
+
+			return true, nil
+		}
+		glog.V(100).Infof("failed to get pod %s/%s: %v", builder.Definition.Namespace, builder.Definition.Name, err)
+
+		return false, err
+	})
+
+	return err
+}
+
+// ExecCommand runs command in the pod and returns buffer output.
+func (builder *Builder) ExecCommand(command []string, containerName ...string) (bytes.Buffer, error) {
+	var (
+		buffer bytes.Buffer
+		cName  string
+	)
+
+	if len(containerName) > 0 {
+		cName = containerName[0]
+	} else {
+		cName = builder.Definition.Spec.Containers[0].Name
+	}
+
+	req := builder.apiClient.CoreV1Interface.RESTClient().
+		Post().
+		Namespace(builder.Object.Namespace).
+		Resource("pods").
+		Name(builder.Object.Name).
+		SubResource("exec").
+		VersionedParams(&v1.PodExecOptions{
+			Container: cName,
+			Command:   command,
+			Stdin:     true,
+			Stdout:    true,
+			Stderr:    true,
+			TTY:       true,
+		}, scheme.ParameterCodec)
+
+	exec, err := remotecommand.NewSPDYExecutor(builder.apiClient.Config, "POST", req.URL())
+
+	if err != nil {
+		return buffer, err
+	}
+
+	err = exec.Stream(remotecommand.StreamOptions{
+		Stdin:  os.Stdin,
+		Stdout: &buffer,
+		Stderr: os.Stderr,
+		Tty:    true,
+	})
+
+	if err != nil {
+		return buffer, err
+	}
+
+	return buffer, nil
+}
+
+// Exists tells whether the given namespace exists.
+func (builder *Builder) Exists() bool {
+	var err error
+	builder.Object, err = builder.apiClient.Pods(builder.Definition.Namespace).Get(
+		context.Background(), builder.Definition.Name, metaV1.GetOptions{})
+
+	return err == nil || !k8serrors.IsNotFound(err)
+}
+
+func getDefinition(name, nsName, image string) *v1.Pod {
+	return &v1.Pod{
+		ObjectMeta: metaV1.ObjectMeta{
+			GenerateName: name,
+			Namespace:    nsName},
+		Spec: v1.PodSpec{
+			TerminationGracePeriodSeconds: pointer.Int64Ptr(0),
+			Containers: []v1.Container{
+				{
+					Name:    "test",
+					Image:   image,
+					Command: []string{"/bin/bash", "-c", "sleep INF"},
+				},
+			},
+		},
+	}
+}
