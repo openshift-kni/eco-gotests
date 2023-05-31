a6b51d04815cd802ac373511c8f8cdb14f7ac552
Alexander Chuzhoy <achuzhoy@users.noreply.github.com>
2023-05-31 10:36:14 -0400

    TC to create an agentserviceconfig without imagestorage (#154)
    This MR automates creation of agentserviceconfig without
    imagestorage with ginkgo.
    
    Signed-off-by: Alexander Chuzhoy <achuzhoy@redhat.com>

--- tests/assisted/ztp/operator/tests/image-service-statefulset.go
+++ tests/assisted/ztp/operator/tests/image-service-statefulset.go
@@ -2,6 +2,7 @@ package operator_test
 
 import (
 	"fmt"
+	"time"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
@@ -11,12 +12,16 @@ import (
 	. "github.com/openshift-kni/eco-gotests/tests/assisted/ztp/internal/ztpinittools"
 	"github.com/openshift-kni/eco-gotests/tests/assisted/ztp/operator/internal/tsparams"
 	"github.com/openshift-kni/eco-gotests/tests/internal/polarion"
+	"github.com/openshift/assisted-service/api/v1beta1"
+	v1 "k8s.io/api/core/v1"
 )
 
 var (
-	agentServiceConfigBuilder             *assisted.AgentServiceConfigBuilder
-	pvcbuilder                            *storage.PVCBuilder
-	imageServicePersistentVolumeClaimName string = "image-service-data-assisted-image-service-0"
+	agentServiceConfigBuilder, tempAgentServiceConfigBuilder *assisted.AgentServiceConfigBuilder
+	pvcbuilder                                               *storage.PVCBuilder
+	imageServicePersistentVolumeClaimName                    string = "image-service-data-assisted-image-service-0"
+	databaseStorage, fileSystemStorage                       v1.PersistentVolumeClaimSpec
+	osImage                                                  []v1beta1.OSImage
 )
 var _ = Describe(
 	"ImageServiceStatefulset",
@@ -31,26 +36,87 @@ var _ = Describe(
 					Skip(msg)
 				}
 
-				By("Assure AgentServiceConfig exists")
+				By("Retrieve the pre-existing AgentServiceConfig")
 				var err error
 				agentServiceConfigBuilder, err = assisted.PullAgentServiceConfig(HubAPIClient)
 				Expect(err).ShouldNot(HaveOccurred(), "Failed to get AgentServiceConfig.")
 
-				By("Assure PVC exists")
-				pvcbuilder, err = storage.PullPersistentVolumeClaim(
-					HubAPIClient, imageServicePersistentVolumeClaimName, tsparams.MCENameSpace)
-				Expect(err).ShouldNot(
-					HaveOccurred(),
-					fmt.Sprintf(
+				By("Initialize variables for the test from the original AgentServiceConfig")
+				databaseStorage = agentServiceConfigBuilder.Definition.Spec.DatabaseStorage
+				fileSystemStorage = agentServiceConfigBuilder.Definition.Spec.FileSystemStorage
+				osImage = agentServiceConfigBuilder.Object.Spec.OSImages
+
+				By("Delete the pre-existing AgentServiceConfig")
+				err = agentServiceConfigBuilder.DeleteAndWait(time.Second * 10)
+				Expect(err).ToNot(HaveOccurred(), "error deleting pre-existing agentserviceconfig")
+
+			})
+			AfterEach(func() {
+				By("Delete AgentServiceConfig after test")
+				err = tempAgentServiceConfigBuilder.DeleteAndWait(time.Second * 10)
+				Expect(err).ToNot(HaveOccurred(), "error deleting agentserviceconfig after test")
+			})
+			AfterAll(func() {
+				By("Re-create the original AgentServiceConfig after all tests")
+				_, err = agentServiceConfigBuilder.Create()
+				Expect(err).ToNot(HaveOccurred(), "error re-creating the original agentserviceconfig after all tests")
+
+				_, err = agentServiceConfigBuilder.WaitUntilDeployed(time.Minute * 10)
+				Expect(err).ToNot(HaveOccurred(),
+					"error waiting until agentserviceconfig without imagestorage is deployed")
+			})
+			It("Assert assisted-image-service pvc is created according to imageStorage spec",
+				polarion.ID("49673"), func() {
+					By("Create AgentServiceConfig with default storage specs")
+					tempAgentServiceConfigBuilder = assisted.NewDefaultAgentServiceConfigBuilder(HubAPIClient)
+
+					// An attempt to restrict the osImages spec for the new agentserviceconfig
+					// to prevent the download of all os images
+					if len(osImage) > 0 {
+						_, err = tempAgentServiceConfigBuilder.WithOSImage(osImage[0]).Create()
+					} else {
+						_, err = tempAgentServiceConfigBuilder.Create()
+					}
+					Expect(err).ToNot(HaveOccurred(),
+						"error creating agentserviceconfig with default storage specs")
+
+					By("Assure the AgentServiceConfig with default storage specs was successfully created")
+					_, err = tempAgentServiceConfigBuilder.WaitUntilDeployed(time.Minute * 10)
+					Expect(err).ToNot(HaveOccurred(),
+						"error waiting until agentserviceconfig with default storage specs is deployed")
+
+					By("Assure PVC exists")
+					pvcbuilder, err = storage.PullPersistentVolumeClaim(
+						HubAPIClient, imageServicePersistentVolumeClaimName, tsparams.MCENameSpace)
+					Expect(err).ShouldNot(HaveOccurred(), fmt.Sprintf(
 						"Failed to get PersistentVolumeClaim %s in NameSpace %s.",
 						imageServicePersistentVolumeClaimName,
 						tsparams.MCENameSpace))
-			})
 
-			It("Assert assisted-image-service pvc is created according to imageStorage spec",
-				polarion.ID("49673"), func() {
+					By("Assure PVC spec matches the value set in AgentServiceConfig")
 					Expect(pvcbuilder.Object.Spec.Resources.Requests.Storage().String()).To(
-						Equal(agentServiceConfigBuilder.Object.Spec.ImageStorage.Resources.Requests.Storage().String()))
+						Equal(tempAgentServiceConfigBuilder.Object.Spec.ImageStorage.Resources.Requests.Storage().String()))
+				})
+
+			It("Assert assisted-image-service is healthy without imageStorage defined in AgentServiceConfig",
+				polarion.ID("49674"), func() {
+					By("Create an AgentServiceConfig without ImageStorage")
+					tempAgentServiceConfigBuilder = assisted.NewAgentServiceConfigBuilder(
+						HubAPIClient, databaseStorage, fileSystemStorage)
+
+					// An attempt to restrict the osImages spec for the new agentserviceconfig
+					// to prevent the download of all os images
+					if len(osImage) > 0 {
+						_, err = tempAgentServiceConfigBuilder.WithOSImage(osImage[0]).Create()
+					} else {
+						_, err = tempAgentServiceConfigBuilder.Create()
+					}
+					Expect(err).ToNot(HaveOccurred(), "error creating agentserviceconfig without imagestorage")
+
+					By("Assure the AgentServiceConfig without ImageStorage was successfully created")
+					_, err = tempAgentServiceConfigBuilder.WaitUntilDeployed(time.Minute * 10)
+					Expect(err).ToNot(HaveOccurred(), "error waiting until agentserviceconfig without imagestorage is deployed")
+					Expect(tempAgentServiceConfigBuilder.Object.Spec.ImageStorage).To(BeNil())
 				})
 		})
 	})
