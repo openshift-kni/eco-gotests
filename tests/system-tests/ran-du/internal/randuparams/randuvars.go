46121607f14fcb6911afc626653e637f229c02e9
Marius Cornea <mcornea@redhat.com>
2023-06-27 23:41:17 +0300

    Add initial system-tests ran-du directory (#198)

---
+++ tests/system-tests/ran-du/internal/randuparams/randuvars.go
@@ -0,0 +1,8 @@
+package randuparams
+
+import "github.com/openshift-kni/eco-gotests/tests/system-tests/internal/systemtestsparams"
+
+var (
+	// Labels represents the range of labels that can be used for test cases selection.
+	Labels = []string{systemtestsparams.Label, Label}
+)
