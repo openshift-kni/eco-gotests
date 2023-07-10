807ac4aa74c2cbb8efb297a1ecf25f0a84ebcfde
Yurii Prokulevych <yprokule@users.noreply.github.com>
2023-07-10 18:17:05 +0200

    System Tests added spkconfig, inittools and params for SPK tests (#217)

---
+++ tests/system-tests/spk/internal/spkconfig/config.go
@@ -0,0 +1,83 @@
+package spkconfig
+
+import (
+	"log"
+	"os"
+	"path/filepath"
+	"runtime"
+
+	"github.com/kelseyhightower/envconfig"
+	"github.com/openshift-kni/eco-gotests/tests/system-tests/internal/systemtestsconfig"
+	"gopkg.in/yaml.v2"
+)
+
+const (
+	// PathToDefaultSPKParamsFile path to config file with default SPK parameters.
+	PathToDefaultSPKParamsFile = "./default.yaml"
+)
+
+// SPKConfig type keeps SPK configuration.
+type SPKConfig struct {
+	*systemtestsconfig.SystemTestsConfig
+	TestWorkload struct {
+		Namespace string `yaml:"spk_workload_ns" envconfig:"ECO_SYSTEM_SPK_WORKLOAD_NS"`
+	}
+}
+
+// NewSPKConfig returns instance of SPKConfig config type.
+func NewSPKConfig() *SPKConfig {
+	log.Print("Creating new SPKConfig struct")
+
+	var spkConf SPKConfig
+	spkConf.SystemTestsConfig = systemtestsconfig.NewSystemTestsConfig()
+
+	_, filename, _, _ := runtime.Caller(0)
+	baseDir := filepath.Dir(filename)
+	confFile := filepath.Join(baseDir, PathToDefaultSPKParamsFile)
+	err := readFile(&spkConf, confFile)
+
+	if err != nil {
+		log.Printf("Error to read config file %s", confFile)
+
+		return nil
+	}
+
+	err = readEnv(&spkConf)
+
+	if err != nil {
+		log.Print("Error to read environment variables")
+
+		return nil
+	}
+
+	return &spkConf
+}
+
+func readFile(spkConfig *SPKConfig, cfgFile string) error {
+	openedCfgFile, err := os.Open(cfgFile)
+	if err != nil {
+		return err
+	}
+
+	defer func() {
+		_ = openedCfgFile.Close()
+	}()
+
+	decoder := yaml.NewDecoder(openedCfgFile)
+	err = decoder.Decode(&spkConfig)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func readEnv(spkConfig *SPKConfig) error {
+	err := envconfig.Process("", spkConfig)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
---
+++ tests/system-tests/spk/internal/spkparams/const.go
@@ -0,0 +1,6 @@
+package spkparams
+
+const (
+	// Label is used to select system tests for SPK operator.
+	Label = "spk"
+)
