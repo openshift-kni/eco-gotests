cfe7c7a65d6ae3f5b747b13df5a1a1e183fd4666
Polina Rabinovich <58551443+prabinovRedhat@users.noreply.github.com>
2023-07-19 18:12:52 +0300

    Add RHWA config & params (#234)
    * finish
    
    * fix
    
    * add RHWA config and params
    
    * remove snr, nhc, mdr, nmo, far config dir

---
+++ tests/rhwa/nmo-operator/internal/nmoparams/nmovars.go
@@ -0,0 +1,8 @@
+package nmoparams
+
+import "github.com/openshift-kni/eco-gotests/tests/rhwa/internal/rhwaparams"
+
+var (
+	// Labels represents the range of labels that can be used for test cases selection.
+	Labels = []string{rhwaparams.Label, Label}
+)
