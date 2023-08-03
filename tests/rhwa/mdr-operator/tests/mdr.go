b14f51a1d7d3cdabdce3775bda90b3667174009e
Polina Rabinovich <58551443+prabinovRedhat@users.noreply.github.com>
2023-08-03 04:53:37 +0300

    Add MDR post deployment verification test (#258)
    * add mdr first test
    
    * fix errors
    
    * fix imports
    
    * fix
    
    * fix
    
    * update polarion reporter fix to mdr_suite_test

---
+++ tests/rhwa/mdr-operator/tests/mdr.go
@@ -0,0 +1,42 @@
+package tests
+
+import (
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+
+	"github.com/openshift-kni/eco-goinfra/pkg/deployment"
+	"github.com/openshift-kni/eco-goinfra/pkg/pod"
+
+	"github.com/openshift-kni/eco-gotests/tests/internal/polarion"
+
+	. "github.com/openshift-kni/eco-gotests/tests/rhwa/internal/rhwainittools"
+	"github.com/openshift-kni/eco-gotests/tests/rhwa/internal/rhwaparams"
+	"github.com/openshift-kni/eco-gotests/tests/rhwa/mdr-operator/internal/mdrparams"
+
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+var _ = Describe(
+	"MDR tests",
+	Ordered,
+	ContinueOnFailure,
+	Label(mdrparams.Label), func() {
+		BeforeAll(func() {
+			By("Get MDR deployment object")
+			mdrDeployment, err := deployment.Pull(
+				APIClient, mdrparams.OperatorDeploymentName, rhwaparams.RhwaOperatorNs)
+			Expect(err).ToNot(HaveOccurred(), "Failed to get MDR deployment")
+
+			By("Verify MDR deployment is Ready")
+			Expect(mdrDeployment.IsReady(rhwaparams.DefaultTimeout)).To(BeTrue(), "MDR deployment is not Ready")
+		})
+		It("Verify Machine Deletion Remediation Operator pod is running", polarion.ID("65767"), func() {
+			_, err := pod.WaitForAllPodsInNamespaceRunning(
+				APIClient,
+				rhwaparams.RhwaOperatorNs,
+				v1.ListOptions{},
+				rhwaparams.DefaultTimeout,
+			)
+			Expect(err).ToNot(HaveOccurred(), "Pod is not ready")
+		})
+	})
