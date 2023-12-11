bbf3c8294c8d9db09f3a9d1d9e0989184c82d272
Trey West <treywest45th@gmail.com>
2023-12-11 14:35:39 -0500

    assisted ztp: update build-artifact-rootfs to use setup pkg (#457)

--- tests/assisted/ztp/operator/tests/build-artifacts-rootfs.go
+++ tests/assisted/ztp/operator/tests/build-artifacts-rootfs.go
@@ -11,32 +11,23 @@ import (
 	"github.com/cavaliergopher/cpio"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
-	"github.com/openshift-kni/eco-goinfra/pkg/assisted"
 	"github.com/openshift-kni/eco-goinfra/pkg/hive"
-	"github.com/openshift-kni/eco-goinfra/pkg/namespace"
-	"github.com/openshift-kni/eco-goinfra/pkg/secret"
 	"github.com/openshift-kni/eco-gotests/tests/assisted/internal/url"
 	"github.com/openshift-kni/eco-gotests/tests/assisted/ztp/internal/meets"
+	"github.com/openshift-kni/eco-gotests/tests/assisted/ztp/internal/setup"
 	. "github.com/openshift-kni/eco-gotests/tests/assisted/ztp/internal/ztpinittools"
 	"github.com/openshift-kni/eco-gotests/tests/assisted/ztp/operator/internal/tsparams"
 	"github.com/openshift-kni/eco-gotests/tests/internal/polarion"
-	"github.com/openshift/assisted-service/api/hiveextension/v1beta1"
-	v1 "k8s.io/api/core/v1"
-	metaV1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 const (
-	rootfsTestName    = "rootfs-test"
+	rootfsSpokeName   = "rootfs-test"
 	rootfsDownloadDir = "/tmp/assistedrootfs"
 )
 
 var (
-	rootfsTestNS                  *namespace.Builder
-	rootfsTestPullSecret          *secret.Builder
-	rootfsTestClusterDeployment   *hive.ClusterDeploymentBuilder
-	rootfsTestAgentClusterInstall *assisted.AgentClusterInstallBuilder
-	rootfsTestInfraEnv            *assisted.InfraEnvBuilder
-	rootfsISOId, rootfsImgID      string
+	rootfsSpokeResources     *setup.SpokeClusterResources
+	rootfsISOId, rootfsImgID string
 )
 
 var _ = Describe(
@@ -59,58 +50,13 @@ var _ = Describe(
 					}
 				}
 
-				By("Create test namespace")
-				rootfsTestNS, err = namespace.NewBuilder(HubAPIClient, rootfsTestName).Create()
-				Expect(err).ToNot(HaveOccurred(), "error creating %s namespace", rootfsTestName)
+				By("Creating " + rootfsSpokeName + " spoke cluster resources")
+				rootfsSpokeResources, err = setup.NewSpokeCluster(HubAPIClient).WithName(rootfsSpokeName).WithDefaultNamespace().
+					WithDefaultPullSecret().WithDefaultClusterDeployment().
+					WithDefaultIPv4AgentClusterInstall().WithDefaultInfraEnv().Create()
+				Expect(err).ToNot(HaveOccurred(), "error creating %s spoke resources", rootfsSpokeName)
 
-				By("Create test pull-secret")
-				rootfsTestPullSecret, err = secret.NewBuilder(
-					HubAPIClient,
-					rootfsTestName,
-					rootfsTestName,
-					v1.SecretTypeDockerConfigJson).WithData(ZTPConfig.HubPullSecret.Definition.Data).Create()
-				Expect(err).ToNot(HaveOccurred(), "error creating %s pull-secret", rootfsTestName)
-
-				By("Create test clusterdeployment")
-				rootfsTestClusterDeployment, err = hive.NewABMClusterDeploymentBuilder(HubAPIClient,
-					rootfsTestName,
-					rootfsTestName,
-					rootfsTestName,
-					"qe.redhat.com",
-					rootfsTestName,
-					metaV1.LabelSelector{MatchLabels: map[string]string{
-						"dummy": "label",
-					}}).WithPullSecret(rootfsTestName).Create()
-
-				Expect(err).ToNot(HaveOccurred(), "error creating %s clusterdeployment", rootfsTestName)
-
-				By("Create test agentclusterinstall")
-				rootfsTestAgentClusterInstall, err = assisted.NewAgentClusterInstallBuilder(HubAPIClient,
-					rootfsTestName,
-					rootfsTestName,
-					rootfsTestName,
-					3,
-					2,
-					v1beta1.Networking{ClusterNetwork: []v1beta1.ClusterNetworkEntry{{
-						CIDR:       "10.128.0.0/14",
-						HostPrefix: 23,
-					}},
-						MachineNetwork: []v1beta1.MachineNetworkEntry{{
-							CIDR: "192.168.254.0/24",
-						}},
-						ServiceNetwork: []string{"172.30.0.0/16"}}).WithImageSet(ZTPConfig.HubOCPXYVersion).Create()
-
-				Expect(err).ToNot(HaveOccurred(), "error creating %s agentclusterinstall", rootfsTestName)
-
-				By("Create test infraenv")
-				rootfsTestInfraEnv, err = assisted.NewInfraEnvBuilder(
-					HubAPIClient,
-					rootfsTestName,
-					rootfsTestName,
-					rootfsTestName).WithClusterRef(rootfsTestName, rootfsTestName).Create()
-				Expect(err).ToNot(HaveOccurred(), "error creating %s infraenv", rootfsTestName)
-
-				_, err = rootfsTestInfraEnv.WaitForDiscoveryISOCreation(time.Minute * 3)
+				_, err = rootfsSpokeResources.InfraEnv.WaitForDiscoveryISOCreation(time.Minute * 3)
 				Expect(err).ToNot(HaveOccurred(), "error waiting for download url to be created")
 
 				if _, err = os.Stat(rootfsDownloadDir); err != nil {
@@ -121,10 +67,11 @@ var _ = Describe(
 				err = os.Mkdir(rootfsDownloadDir, 0755)
 				Expect(err).ToNot(HaveOccurred(), "error creating downloads directory")
 
-				err = url.DownloadToDir(rootfsTestInfraEnv.Object.Status.ISODownloadURL, rootfsDownloadDir, true)
+				err = url.DownloadToDir(rootfsSpokeResources.InfraEnv.Object.Status.ISODownloadURL, rootfsDownloadDir, true)
 				Expect(err).ToNot(HaveOccurred(), "error downloading ISO")
 
-				err = url.DownloadToDir(rootfsTestInfraEnv.Object.Status.BootArtifacts.RootfsURL, rootfsDownloadDir, true)
+				err = url.DownloadToDir(rootfsSpokeResources.InfraEnv.Object.Status.BootArtifacts.RootfsURL,
+					rootfsDownloadDir, true)
 				Expect(err).ToNot(HaveOccurred(), "error downloading rootfs")
 
 				dirEntry, err := os.ReadDir(rootfsDownloadDir)
@@ -165,20 +112,8 @@ var _ = Describe(
 			)
 
 			AfterAll(func() {
-				err = rootfsTestInfraEnv.Delete()
-				Expect(err).ToNot(HaveOccurred(), "error removing infraenv")
-
-				err = rootfsTestAgentClusterInstall.Delete()
-				Expect(err).ToNot(HaveOccurred(), "error removing agentclusterinstall")
-
-				err = rootfsTestClusterDeployment.Delete()
-				Expect(err).ToNot(HaveOccurred(), "error removing clusterdeployment")
-
-				err = rootfsTestPullSecret.Delete()
-				Expect(err).ToNot(HaveOccurred(), "error removing pull-secret")
-
-				err = rootfsTestNS.Delete()
-				Expect(err).ToNot(HaveOccurred(), "error removing namespace")
+				err = rootfsSpokeResources.Delete()
+				Expect(err).ToNot(HaveOccurred(), "error deleting %s spoke resources", rootfsSpokeName)
 
 				err = os.RemoveAll(rootfsDownloadDir)
 				Expect(err).ToNot(HaveOccurred(), "error removing download dir")
