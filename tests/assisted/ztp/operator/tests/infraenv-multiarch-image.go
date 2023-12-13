2e7d83c7ea9f22110a8cabd943b3089cf6a17ce1
Alexander Chuzhoy <achuzhoy@users.noreply.github.com>
2023-12-13 09:28:49 -0500

    assisted ztp: checking conditions of infraenv instead of aci (#454)

--- tests/assisted/ztp/operator/tests/infraenv-multiarch-image.go
+++ tests/assisted/ztp/operator/tests/infraenv-multiarch-image.go
@@ -1,6 +1,7 @@
 package operator_test
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"io"
@@ -13,6 +14,7 @@ import (
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	"github.com/openshift-kni/eco-goinfra/pkg/assisted"
+	"github.com/openshift-kni/eco-goinfra/pkg/configmap"
 	"github.com/openshift-kni/eco-goinfra/pkg/hive"
 	"github.com/openshift-kni/eco-goinfra/pkg/namespace"
 	"github.com/openshift-kni/eco-goinfra/pkg/secret"
@@ -27,6 +29,7 @@ import (
 	"github.com/openshift/assisted-service/models"
 	v1 "k8s.io/api/core/v1"
 	metaV1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/wait"
 )
 
 const (
@@ -38,9 +41,9 @@ var (
 	nsBuilder                                                  *namespace.Builder
 	archURL                                                    string
 	platformType                                               v1beta1.PlatformType
-	userManagedNetworking                                      bool
 	originalClusterImagesetBuilder, tempClusterImagesetBuilder *hive.ClusterImageSetBuilder
 	mirrorRegistryRef                                          *v1.LocalObjectReference
+	configmapBuilder                                           *configmap.Builder
 )
 var _ = Describe(
 	"MultiArchitectureImage",
@@ -78,6 +81,69 @@ var _ = Describe(
 						testClusterImageSetName)
 				}
 
+				By("Create configmap for AgentServiceConfig's annotation")
+				agentServiceConfigMapData := map[string]string{
+					"ISO_IMAGE_TYPE": "full-iso",
+				}
+				configmapBuilder = configmap.NewBuilder(HubAPIClient, "assisted-unsupported-config",
+					ZTPConfig.HubAssistedServicePod().Object.Namespace)
+				_, err = configmapBuilder.WithData(agentServiceConfigMapData).Create()
+				Expect(err).ToNot(HaveOccurred(), "error creating configmap for AgentServiceConfig's annotation")
+
+				By("Create AgentServiceConfig with OS images for multiple architectures")
+				tempAgentServiceConfigBuilder = assisted.NewDefaultAgentServiceConfigBuilder(HubAPIClient)
+
+				// Add annotation to be able to use full ISO with s390x arch
+				tempAgentServiceConfigBuilder.Definition.Annotations = map[string](string){}
+				tempAgentServiceConfigBuilder.Definition.
+					Annotations["unsupported.agent-install.openshift.io/assisted-service-configmap"] =
+					"assisted-unsupported-config"
+
+				if mirrorRegistryRef != nil {
+					tempAgentServiceConfigBuilder.Definition.Spec.MirrorRegistryRef = mirrorRegistryRef
+				}
+				_, err = tempAgentServiceConfigBuilder.WithOSImage(agentInstallV1Beta1.OSImage{
+					OpenshiftVersion: ZTPConfig.HubOCPXYVersion,
+					Version:          ZTPConfig.HubOCPXYVersion,
+					Url: getArchURL(
+						"https://mirror.openshift.com/pub/openshift-v4/amd64/dependencies/rhcos/"+
+							ZTPConfig.HubOCPXYVersion+"/latest/rhcos-live.x86_64.iso",
+						"https://mirror.openshift.com/pub/openshift-v4/amd64/dependencies/rhcos/pre-release/latest-"+
+							ZTPConfig.HubOCPXYVersion+"/rhcos-live.x86_64.iso"),
+					CPUArchitecture: models.ClusterCPUArchitectureX8664}).WithOSImage(agentInstallV1Beta1.OSImage{
+					OpenshiftVersion: ZTPConfig.HubOCPXYVersion,
+					Version:          ZTPConfig.HubOCPXYVersion,
+					Url: getArchURL(
+						"https://mirror.openshift.com/pub/openshift-v4/aarch64/dependencies/rhcos/"+
+							ZTPConfig.HubOCPXYVersion+"/latest/rhcos-live.aarch64.iso",
+						"https://mirror.openshift.com/pub/openshift-v4/aarch64/dependencies/rhcos/pre-release/latest-"+
+							ZTPConfig.HubOCPXYVersion+"/rhcos-live.aarch64.iso"),
+					CPUArchitecture: models.ClusterCPUArchitectureArm64}).WithOSImage(agentInstallV1Beta1.OSImage{
+					OpenshiftVersion: ZTPConfig.HubOCPXYVersion,
+					Version:          ZTPConfig.HubOCPXYVersion,
+					Url: getArchURL(
+						"https://mirror.openshift.com/pub/openshift-v4/ppc64le/dependencies/rhcos/"+
+							ZTPConfig.HubOCPXYVersion+"/latest/rhcos-live.ppc64le.iso",
+						"https://mirror.openshift.com/pub/openshift-v4/ppc64le/dependencies/rhcos/pre-release/latest-"+
+							ZTPConfig.HubOCPXYVersion+"/rhcos-live.ppc64le.iso"),
+					CPUArchitecture: models.ClusterCPUArchitecturePpc64le}).WithOSImage(agentInstallV1Beta1.OSImage{
+					OpenshiftVersion: ZTPConfig.HubOCPXYVersion,
+					Version:          ZTPConfig.HubOCPXYVersion,
+					Url: getArchURL(
+						"https://mirror.openshift.com/pub/openshift-v4/s390x/dependencies/rhcos/"+
+							ZTPConfig.HubOCPXYVersion+"/latest/rhcos-live.s390x.iso",
+						"https://mirror.openshift.com/pub/openshift-v4/s390x/dependencies/rhcos/pre-release/latest-"+
+							ZTPConfig.HubOCPXYVersion+"/rhcos-live.s390x.iso"),
+					CPUArchitecture: models.ClusterCPUArchitectureS390x}).
+					Create()
+				Expect(err).ToNot(HaveOccurred(),
+					"error creating agentserviceconfig with osimages for multiple archs")
+
+				By("Wait until AgentServiceConfig with OSImages for multiple archs is deployed")
+				_, err = tempAgentServiceConfigBuilder.WaitUntilDeployed(time.Minute * 10)
+				Expect(err).ToNot(HaveOccurred(),
+					"error waiting until agentserviceconfig with osimages for multiple archs is deployed")
+
 			})
 			AfterEach(func() {
 				By("Delete the temporary namespace after test")
@@ -91,9 +157,6 @@ var _ = Describe(
 				Expect(err).ToNot(HaveOccurred(), "error deleting clusterimageset %s", testClusterImageSetName)
 				glog.V(ztpparams.ZTPLogLevel).Infof("The ClusterImageSet %s was deleted", testClusterImageSetName)
 
-				By("Delete AgentServiceConfig after test")
-				err = tempAgentServiceConfigBuilder.DeleteAndWait(time.Second * 10)
-				Expect(err).ToNot(HaveOccurred(), "error deleting agentserviceconfig after test")
 			})
 			AfterAll(func() {
 				By("Re-create the original ClusterImageSet after all test if existed")
@@ -105,6 +168,13 @@ var _ = Describe(
 				} else {
 					glog.V(ztpparams.ZTPLogLevel).Infof("Skip on re-creating the ClusterImageSet after all tests - didn't exist.")
 				}
+				By("Delete AgentServiceConfig after test")
+				err = tempAgentServiceConfigBuilder.DeleteAndWait(time.Second * 10)
+				Expect(err).ToNot(HaveOccurred(), "error deleting agentserviceconfig after test")
+
+				By("Delete ConfigMap after test")
+				err = configmapBuilder.Delete()
+				Expect(err).ToNot(HaveOccurred(), "error deleting configmap after test")
 
 				By("Re-create the original AgentServiceConfig after all tests")
 				_, err = ZTPConfig.HubAgentServiceConfg.Create()
@@ -116,49 +186,7 @@ var _ = Describe(
 
 			})
 			DescribeTable("Assert valid ISO is created by InfraEnv with different cpuArchitecture",
-				func(archUrlReleased string, archUrlPrereleased string, cpuArchitecture string,
-					payloadURL string, mismatchCPUArchitecture ...string) {
-
-					By("Check the rhcos-live ISO exists")
-					archURL = archUrlReleased
-
-					_, statusCode, err := url.Fetch(archURL, "Head", true)
-					if err != nil || statusCode != 200 {
-						archURL = archUrlPrereleased
-						_, statusCode, err = url.Fetch(archURL, "Head", true)
-					}
-					Expect(statusCode).To(Equal(200), "bad HTTP status from url %s", archURL)
-					Expect(err).ToNot(HaveOccurred(), "error reaching %s", archURL)
-
-					glog.V(ztpparams.ZTPLogLevel).Infof("Verified ISO from URL %s exists", archURL)
-
-					By("Create AgentServiceConfig with the specific OSImage")
-
-					tempAgentServiceConfigBuilder = assisted.NewDefaultAgentServiceConfigBuilder(HubAPIClient)
-
-					if mirrorRegistryRef != nil {
-						tempAgentServiceConfigBuilder.Definition.Spec.MirrorRegistryRef = mirrorRegistryRef
-					}
-					if len(mismatchCPUArchitecture) > 0 {
-						_, err = tempAgentServiceConfigBuilder.WithOSImage(agentInstallV1Beta1.OSImage{
-							OpenshiftVersion: ZTPConfig.HubOCPXYVersion,
-							Version:          ZTPConfig.HubOCPXYVersion,
-							Url:              archURL,
-							CPUArchitecture:  mismatchCPUArchitecture[0]}).Create()
-					} else {
-						_, err = tempAgentServiceConfigBuilder.WithOSImage(agentInstallV1Beta1.OSImage{
-							OpenshiftVersion: ZTPConfig.HubOCPXYVersion,
-							Version:          ZTPConfig.HubOCPXYVersion,
-							Url:              archURL,
-							CPUArchitecture:  cpuArchitecture}).Create()
-					}
-					Expect(err).ToNot(HaveOccurred(),
-						"error creating agentserviceconfig with the specific osimage")
-
-					By("Wait until AgentServiceConfig with the specific OSImage is deployed")
-					_, err = tempAgentServiceConfigBuilder.WaitUntilDeployed(time.Minute * 10)
-					Expect(err).ToNot(HaveOccurred(),
-						"error waiting until agentserviceconfig without imagestorage is deployed")
+				func(cpuArchitecture string, payloadURL string, mismatchCPUArchitecture ...string) {
 
 					By("Create ClusterImageSet")
 					payloadVersion, payloadImage, err := getLatestReleasePayload(payloadURL)
@@ -182,54 +210,54 @@ var _ = Describe(
 				},
 
 				Entry("Assert valid ISO is created by InfraEnv with cpuArchitecture set to x86_64",
-					"https://mirror.openshift.com/pub/openshift-v4/amd64/dependencies/rhcos/"+
-						ZTPConfig.HubOCPXYVersion+"/latest/rhcos-live.x86_64.iso",
-					"https://mirror.openshift.com/pub/openshift-v4/amd64/dependencies/rhcos/pre-release/latest-"+
-						ZTPConfig.HubOCPXYVersion+"/rhcos-live.x86_64.iso",
 					models.ClusterCPUArchitectureX8664,
 					"https://openshift-release.apps.ci.l2s4.p1.openshiftapps.com/graph",
 					polarion.ID("56183")),
 
 				Entry("Assert valid ISO is created by InfraEnv with cpuArchitecture set to s390x",
-					"https://mirror.openshift.com/pub/openshift-v4/s390x/dependencies/rhcos/"+
-						ZTPConfig.HubOCPXYVersion+"/latest/rhcos-live.s390x.iso",
-					"https://mirror.openshift.com/pub/openshift-v4/s390x/dependencies/rhcos/pre-release/latest-"+
-						ZTPConfig.HubOCPXYVersion+"/rhcos-live.s390x.iso",
 					models.ClusterCPUArchitectureS390x,
 					"https://s390x.ocp.releases.ci.openshift.org/graph",
 					polarion.ID("56184")),
 
 				Entry("Assert valid ISO is created by InfraEnv with cpuArchitecture set to ppc64le",
-					"https://mirror.openshift.com/pub/openshift-v4/ppc64le/dependencies/rhcos/"+
-						ZTPConfig.HubOCPXYVersion+"/latest/rhcos-live.ppc64le.iso",
-					"https://mirror.openshift.com/pub/openshift-v4/ppc64le/dependencies/rhcos/pre-release/latest-"+
-						ZTPConfig.HubOCPXYVersion+"/rhcos-live.ppc64le.iso",
 					models.ClusterCPUArchitecturePpc64le,
 					"https://ppc64le.ocp.releases.ci.openshift.org/graph",
 					polarion.ID("56185")),
 
 				Entry("Assert valid ISO is created by InfraEnv with cpuArchitecture set to arm64",
-					"https://mirror.openshift.com/pub/openshift-v4/aarch64/dependencies/rhcos/"+
-						ZTPConfig.HubOCPXYVersion+"/latest/rhcos-live.aarch64.iso",
-					"https://mirror.openshift.com/pub/openshift-v4/aarch64/dependencies/rhcos/pre-release/latest-"+
-						ZTPConfig.HubOCPXYVersion+"/rhcos-live.aarch64.iso",
 					models.ClusterCPUArchitectureArm64,
 					"https://arm64.ocp.releases.ci.openshift.org/graph",
 					polarion.ID("56186")),
 
-				Entry("Assert the InfraEnv creation fails when the cpuArchitecture is mismatched with ppc64le arch",
-					"https://mirror.openshift.com/pub/openshift-v4/aarch64/dependencies/rhcos/"+
-						ZTPConfig.HubOCPXYVersion+"/latest/rhcos-live.aarch64.iso",
-					"https://mirror.openshift.com/pub/openshift-v4/aarch64/dependencies/rhcos/pre-release/latest-"+
-						ZTPConfig.HubOCPXYVersion+"/rhcos-live.aarch64.iso",
+				Entry("Assert the InfraEnv creation fails when the cpuArchitecture is mismatched with arm64 arch",
+					models.ClusterCPUArchitectureX8664,
+					"https://openshift-release.apps.ci.l2s4.p1.openshiftapps.com/graph",
 					models.ClusterCPUArchitectureArm64,
-					"https://arm64.ocp.releases.ci.openshift.org/graph",
-					models.ClusterCPUArchitecturePpc64le,
 					polarion.ID("56190")),
 			)
 		})
 
 	})
+
+// getArchURL returns the proper ISO image URL.
+func getArchURL(archURLReleased string, archURLPrereleased string) string {
+	By("Check the rhcos-live ISO exists")
+
+	archURL = archURLReleased
+
+	_, statusCode, err := url.Fetch(archURL, "Head", true)
+	if err != nil || statusCode != 200 {
+		archURL = archURLPrereleased
+		_, statusCode, err = url.Fetch(archURL, "Head", true)
+	}
+
+	Expect(statusCode).To(Equal(200), "bad HTTP status from url %s", archURL)
+	Expect(err).ToNot(HaveOccurred(), "error reaching %s", archURL)
+
+	glog.V(ztpparams.ZTPLogLevel).Infof("Verified ISO from URL %s exists", archURL)
+
+	return archURL
+}
 
 // createSpokeClusterNamespace is a helper function that creates namespace
 // for the spoke cluster.
@@ -277,7 +305,7 @@ func createSpokeClusterResources(cpuArch string, mismatchCPUArchitecture ...stri
 
 	By("Create agentclusterinstall in the new namespace")
 
-	agentClusterInstallBuilder, err := assisted.NewAgentClusterInstallBuilder(
+	_, err = assisted.NewAgentClusterInstallBuilder(
 		HubAPIClient,
 		infraenvTestSpoke,
 		infraenvTestSpoke,
@@ -289,13 +317,10 @@ func createSpokeClusterResources(cpuArch string, mismatchCPUArchitecture ...stri
 				CIDR:       "10.128.0.0/14",
 				HostPrefix: 23,
 			}},
-			MachineNetwork: []v1beta1.MachineNetworkEntry{{
-				CIDR: "192.168.254.0/24",
-			}},
 			ServiceNetwork: []string{"172.30.0.0/16"},
 		}).WithImageSet(testClusterImageSetName).
 		WithPlatformType(platformType).
-		WithUserManagedNetworking(userManagedNetworking).Create()
+		WithUserManagedNetworking(true).Create()
 	Expect(err).ToNot(HaveOccurred(), "error creating agentclusterinstall")
 
 	By("Create infraenv in the new namespace")
@@ -310,7 +335,7 @@ func createSpokeClusterResources(cpuArch string, mismatchCPUArchitecture ...stri
 		HubAPIClient,
 		infraenvTestSpoke,
 		infraenvTestSpoke,
-		testSecret.Definition.Name).WithCPUType(cpuArchTemp).Create()
+		testSecret.Definition.Name).WithCPUType(cpuArchTemp).WithClusterRef(infraenvTestSpoke, infraenvTestSpoke).Create()
 
 	Expect(err).ToNot(HaveOccurred(), "error creating infraenv with cpu architecture %s", cpuArchTemp)
 
@@ -320,14 +345,26 @@ func createSpokeClusterResources(cpuArch string, mismatchCPUArchitecture ...stri
 		_, err = infraEnvBuilder.WaitForDiscoveryISOCreation(300 * time.Second)
 		Expect(err).ToNot(HaveOccurred(), "error waiting for the discovery iso creation")
 	} else {
-		By("Wait until ACI shows an error for the mismatching image architecture")
+		By("Wait until infraenv shows an error for the mismatching image architecture")
 
-		err = agentClusterInstallBuilder.WaitForConditionMessage(v1beta1.ClusterSpecSyncedCondition,
-			"The Spec could not be synced due to an input error: Requested CPU architecture "+
-				cpuArch+
-				" is not available",
-			time.Minute,
-		)
+		err = wait.PollUntilContextTimeout(
+			context.TODO(), 5*time.Second, time.Minute*2, true, func(ctx context.Context) (bool, error) {
+				infraEnvBuilder.Object, _ = infraEnvBuilder.Get()
+				if len(infraEnvBuilder.Object.Status.Conditions) > 0 {
+					for _, condition := range infraEnvBuilder.Object.Status.Conditions {
+						if condition.Type == agentInstallV1Beta1.ImageCreatedCondition {
+							if condition.Message ==
+								"Failed to create image: Specified CPU architecture ("+
+									mismatchCPUArchitecture[0]+
+									") doesn't match the cluster ("+cpuArch+")" {
+								return true, nil
+							}
+						}
+					}
+				}
+
+				return false, nil
+			})
 		Expect(err).ToNot(HaveOccurred(), "didn't get the message about mismatching CPU architecture")
 	}
 }
@@ -354,10 +391,12 @@ func getLatestReleasePayload(url string) (string, string, error) {
 	}
 
 	for _, i := range images.Nodes {
-		if strings.Contains(i.Version, ZTPConfig.HubOCPXYVersion) &&
-			!strings.Contains(i.Version, "nightly") &&
-			!strings.Contains(i.Payload, "dev-preview") {
-			releaseImages[i.Version] = i.Payload
+		if len(strings.Split(i.Version, ".")) >= 2 {
+			if strings.Split(i.Version, ".")[0]+"."+strings.Split(i.Version, ".")[1] == ZTPConfig.HubOCPXYVersion &&
+				!strings.Contains(i.Version, "nightly") &&
+				!strings.Contains(i.Payload, "dev-preview") {
+				releaseImages[i.Version] = i.Payload
+			}
 		}
 	}
 
