760ff66d6f613cfeb2a660fcb8939b9c5e9f4540
Marius Cornea <mcornea@redhat.com>
2024-03-25 15:30:09 +0200

    [system-tests][ran-du]Validate SriovFecNodeConfig resource status (#647)

---
+++ tests/system-tests/ran-du/tests/sriovfecnodeconfig-status.go
@@ -0,0 +1,42 @@
+package ran_du_system_test
+
+import (
+	"github.com/golang/glog"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	sriovfec "github.com/openshift-kni/eco-goinfra/pkg/sriov-fec"
+	. "github.com/openshift-kni/eco-gotests/tests/system-tests/ran-du/internal/randuinittools"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	"github.com/openshift-kni/eco-goinfra/pkg/nodes"
+	"github.com/openshift-kni/eco-gotests/tests/system-tests/ran-du/internal/randuparams"
+)
+
+var _ = Describe(
+	"SriovFecNodeConfigStatus",
+	Label("SriovFecNodeConfigStatus"),
+	Ordered,
+	ContinueOnFailure,
+	func() {
+		It("Asserts SriovFecNodeConfig resource is configured successfully", func() {
+			glog.V(randuparams.RanDuLogLevel).Infof("Check SriovFecNodeConfig resource")
+
+			nodeList, err := nodes.List(
+				APIClient,
+				metav1.ListOptions{},
+			)
+			Expect(err).ToNot(HaveOccurred(), "Error listing nodes.")
+
+			for _, node := range nodeList {
+				fecStatus, err := sriovfec.Pull(APIClient, node.Definition.Name, RanDuTestConfig.SriovFecOperatorNamespace)
+				Expect(err).ToNot(HaveOccurred(), "Failed to get SriovFecNodeConfig")
+
+				for _, condition := range fecStatus.Object.Status.Conditions {
+					if condition.Type == "Configured" {
+						Expect(condition.Status).To(BeEquivalentTo("True"), "SriovFecNodeConfig is not configured: %s", condition.Message)
+					}
+				}
+			}
+		})
+	},
+)
