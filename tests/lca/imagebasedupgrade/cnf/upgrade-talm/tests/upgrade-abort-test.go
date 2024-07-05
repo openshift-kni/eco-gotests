94ff118cdb20b4916974097d28f37827005dd969
Marius Cornea <mcornea@redhat.com>
2024-07-05 03:13:12 -0400

    lca: cnf: wait for ibu idle stage after finalize cgu (#79)
    After the finalize CGU reports Completed state wait for the
    IBU Idle condition to report True as well.
    
    Also remove 'Wait until node is reporting as Ready' as it
    can sometimes prematurely fail if the kube api is not available.

--- tests/lca/imagebasedupgrade/cnf/upgrade-talm/tests/upgrade-abort-test.go
+++ tests/lca/imagebasedupgrade/cnf/upgrade-talm/tests/upgrade-abort-test.go
@@ -8,6 +8,7 @@ import (
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	"github.com/openshift-kni/eco-goinfra/pkg/cgu"
+	"github.com/openshift-kni/eco-goinfra/pkg/lca"
 	"github.com/openshift-kni/eco-goinfra/pkg/ocm"
 	"github.com/openshift-kni/eco-goinfra/pkg/reportxml"
 	"github.com/openshift-kni/eco-gotests/tests/lca/imagebasedupgrade/cnf/internal/cnfclusterinfo"
@@ -28,6 +29,9 @@ var _ = Describe(
 				Expect(err).ToNot(HaveOccurred(), "Failed to extract target sno cluster name")
 
 				tsparams.TargetSnoClusterName = cnfclusterinfo.PreUpgradeClusterInfo.Name
+
+				ibu, err = lca.PullImageBasedUpgrade(cnfinittools.TargetSNOAPIClient)
+				Expect(err).NotTo(HaveOccurred(), "error pulling ibu resource from cluster")
 			})
 		})
 
@@ -45,17 +49,16 @@ var _ = Describe(
 				Expect(err).ToNot(HaveOccurred(), "Failed to delete prep cgu on target hub cluster")
 			})
 
-			By("Deleting upgrade cgu created on target hub cluster", func() {
-				err := cnfhelper.DeleteIbuTestCguOnTargetHub(cnfinittools.TargetHubAPIClient, tsparams.UpgradeCguName,
-					tsparams.IbuCguNamespace)
-				Expect(err).ToNot(HaveOccurred(), "Failed to delete upgrade cgu on target hub cluster")
-			})
-
 			By("Deleting finalize cgu created on target hub cluster", func() {
 				err := cnfhelper.DeleteIbuTestCguOnTargetHub(cnfinittools.TargetHubAPIClient, tsparams.FinalizeCguName,
 					tsparams.IbuCguNamespace)
 				Expect(err).ToNot(HaveOccurred(), "Failed to delete finalize cgu on target hub cluster")
 			})
+
+			// Sleep for 10 seconds to allow talm to reconcile state.
+			// Sometimes if the next test re-creates the CGUs too quickly,
+			// the policies compliance status is not updated correctly.
+			time.Sleep(10 * time.Second)
 		})
 
 		It("Abort at IBU upgrade stage", reportxml.ID("69055"), func() {
@@ -85,6 +88,9 @@ var _ = Describe(
 				prepCguBuilder, err := prepCguBuilder.Create()
 				Expect(err).ToNot(HaveOccurred(), "Failed to create prep CGU.")
 
+				_, err = ibu.WaitUntilStageComplete("Prep")
+				Expect(err).NotTo(HaveOccurred(), "error waiting for prep stage to complete")
+
 				_, err = prepCguBuilder.WaitUntilComplete(25 * time.Minute)
 				Expect(err).ToNot(HaveOccurred(), "Prep CGU did not complete in time.")
 			})
@@ -99,9 +105,11 @@ var _ = Describe(
 
 				_, err := upgradeCguBuilder.Create()
 				Expect(err).ToNot(HaveOccurred(), "Failed to create upgrade CGU.")
+				// Wait for 10 seconds to avoid upgrade and finalize CGUs getting created simultaneously.
+				time.Sleep(10 * time.Second)
 			})
 
-			By("Waiting for the upgrade-stage-policy to report NonCompliant state", func() {
+			By("Waiting for the upgrade and finalize policies to report NonCompliant state", func() {
 				upgradeStagePolicy, err := ocm.PullPolicy(cnfinittools.TargetHubAPIClient,
 					tsparams.UpgradePolicyName,
 					tsparams.IbuPolicyNamespace)
@@ -109,6 +117,14 @@ var _ = Describe(
 
 				err = upgradeStagePolicy.WaitUntilComplianceState(policiesv1.NonCompliant, 5*time.Minute)
 				Expect(err).ToNot(HaveOccurred(), "Upgrade-stage-policy failed to report NonCompliant state")
+
+				finalizeStagePolicy, err := ocm.PullPolicy(cnfinittools.TargetHubAPIClient,
+					tsparams.FinalizePolicyName,
+					tsparams.IbuPolicyNamespace)
+				Expect(err).ToNot(HaveOccurred(), "Failed to pull finalize stage policy")
+
+				err = finalizeStagePolicy.WaitUntilComplianceState(policiesv1.NonCompliant, 5*time.Minute)
+				Expect(err).ToNot(HaveOccurred(), "Finalize-stage-policy failed to report NonCompliant state")
 			})
 
 			By("Creating, enabling ibu finalize CGU and waiting for CGU status to report completed", func() {
@@ -119,8 +135,18 @@ var _ = Describe(
 					WithCanary(tsparams.TargetSnoClusterName)
 				finalizeCguBuilder.Definition.Spec.Enable = ptr.To(true)
 
+				// Delete the upgrade CGU so that it does not interfere with the finalize CGU.
+				By("Deleting upgrade cgu created on target hub cluster", func() {
+					err := cnfhelper.DeleteIbuTestCguOnTargetHub(cnfinittools.TargetHubAPIClient, tsparams.UpgradeCguName,
+						tsparams.IbuCguNamespace)
+					Expect(err).ToNot(HaveOccurred(), "Failed to delete upgrade cgu on target hub cluster")
+				})
+
 				finalizeCguBuilder, err := finalizeCguBuilder.Create()
 				Expect(err).ToNot(HaveOccurred(), "Failed to create finalize CGU.")
+
+				_, err = ibu.WaitUntilStageComplete("Idle")
+				Expect(err).NotTo(HaveOccurred(), "error waiting for idle stage to complete")
 
 				_, err = finalizeCguBuilder.WaitUntilComplete(5 * time.Minute)
 				Expect(err).ToNot(HaveOccurred(), "Finalize CGU did not complete in time.")
