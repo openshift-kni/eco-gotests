7b9d15e1d5e959b5b1fecc53734255c36b33293f
Kirsten Laskoski <162511147+klaskosk@users.noreply.github.com>
2024-07-15 13:21:17 -0400

    cnf ran: adding CR reporting to powermanagement (#93)
    This also removes the PrivPodNamespace since it did not actually have any purpose over TestingNamespace.

--- tests/cnf/ran/powermanagement/powermanagement_suite_test.go
+++ tests/cnf/ran/powermanagement/powermanagement_suite_test.go
@@ -7,11 +7,14 @@ import (
 	"github.com/golang/glog"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	"github.com/openshift-kni/eco-goinfra/pkg/clients"
 	"github.com/openshift-kni/eco-goinfra/pkg/namespace"
+	"github.com/openshift-kni/eco-goinfra/pkg/reportxml"
 	. "github.com/openshift-kni/eco-gotests/tests/cnf/ran/internal/raninittools"
 	"github.com/openshift-kni/eco-gotests/tests/cnf/ran/internal/ranparam"
 	"github.com/openshift-kni/eco-gotests/tests/cnf/ran/powermanagement/internal/tsparams"
 	_ "github.com/openshift-kni/eco-gotests/tests/cnf/ran/powermanagement/tests"
+	"github.com/openshift-kni/eco-gotests/tests/internal/reporter"
 )
 
 var _, currentFile, _, _ = runtime.Caller(0)
@@ -44,4 +47,13 @@ var _ = AfterSuite(func() {
 	err := testNamespace.DeleteAndWait(tsparams.PowerSaveTimeout)
 	Expect(err).ToNot(HaveOccurred(), "Failed to delete namespace ", tsparams.TestingNamespace)
 
+})
+
+var _ = JustAfterEach(func() {
+	reporter.ReportIfFailed(
+		CurrentSpecReport(), currentFile, tsparams.ReporterNamespacesToDump, tsparams.ReporterCRsToDump, clients.SetScheme)
+})
+
+var _ = ReportAfterSuite("", func(report Report) {
+	reportxml.Create(report, RANConfig.GetReportPath(), RANConfig.TCPrefix)
 })
