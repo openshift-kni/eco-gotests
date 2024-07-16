ce4253b576d442240b6ba5d5a54bc1b12cd63833
sserafin <sserafin@redhat.com>
2024-07-16 17:33:34 +0200

    accel: add internal folder

---
+++ tests/accel/internal/accelinittools/accelinittools.go
@@ -0,0 +1,22 @@
+package accelinittools
+
+import (
+	"github.com/openshift-kni/eco-goinfra/pkg/clients"
+	"github.com/openshift-kni/eco-gotests/tests/accel/internal/accelconfig"
+	"github.com/openshift-kni/eco-gotests/tests/internal/inittools"
+)
+
+var (
+	// HubAPIClient provides API access to hub cluster.
+	HubAPIClient *clients.Settings
+	// SpokeAPIClient provides API access to spoke cluster.
+	SpokeAPIClient *clients.Settings
+	// AccelConfig provides access to configuration parameters.
+	AccelConfig *accelconfig.AccelConfig
+)
+
+func init() {
+	HubAPIClient = inittools.APIClient
+	SpokeAPIClient = AccelConfig.SpokeAPIClient
+	AccelConfig = accelconfig.NewAccelConfig()
+}
