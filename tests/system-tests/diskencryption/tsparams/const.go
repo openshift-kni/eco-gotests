dfb307fdda1330384837efa7e82741e0b4009cd3
David Elie-Dit-Cosaque <deliedit@redhat.com>
2024-07-31 18:43:42 -0500

    Integrate new functions and bugfix

--- tests/system-tests/diskencryption/tsparams/const.go
+++ tests/system-tests/diskencryption/tsparams/const.go
@@ -17,6 +17,14 @@ const (
 	PollingIntervalBMC = 30 * time.Second
 	// TimeoutWaitingOnBMC timeout until failing a BMC command.
 	TimeoutWaitingOnBMC = 10 * time.Minute
+	// PollingIntervalK8S interval to poll cluster after an error.
+	PollingIntervalK8S = 1 * time.Second
+	// TimeoutWaitingOnK8S timeout until failing a cluster command.
+	TimeoutWaitingOnK8S = 10 * time.Second
 	// LogLevel is the verbosity of glog statements in this test suite.
 	LogLevel glog.Level = 90
+	// RetryInterval retry interval for node exec commands.
+	RetryInterval = 10 * time.Second
+	// RetryCount retry count for node exec commands.
+	RetryCount = 6
 )
