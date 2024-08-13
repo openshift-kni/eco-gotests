540d9ad8fdf5d88abed42a41c84acff576dea92a
Trey West <treywest45th@gmail.com>
2024-08-13 09:00:52 -0400

    ibi mgmt: add directory structure for ibi tests (#138)

---
+++ tests/lca/imagebasedinstall/internal/ibiparams/const.go
@@ -0,0 +1,9 @@
+package ibiparams
+
+const (
+	// Label represents ibi label that can be used for test cases selection.
+	Label = "ibi"
+
+	// IBILogLevel custom loglevel for the imagebasedinstall testing verbose mode.
+	IBILogLevel = 50
+)
