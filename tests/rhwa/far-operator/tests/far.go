b9472a821d1a937935d80346c8ef7f2a848fa765
Francisco Javier Moreno <frajamomo@hotmail.com>
2024-10-18 16:39:23 +0200

    extract prepareRapidastConfig to separate file

--- tests/rhwa/far-operator/tests/far.go
+++ tests/rhwa/far-operator/tests/far.go
@@ -2,10 +2,8 @@ package tests
 
 import (
 	"fmt"
-	"io"
 	"os"
 	"os/exec"
-	"path/filepath"
 	"strings"
 
 	. "github.com/onsi/ginkgo/v2"
@@ -20,7 +18,6 @@ import (
 	"github.com/openshift-kni/eco-gotests/tests/rhwa/internal/rhwaparams"
 
 	"github.com/golang/glog"
-	"gopkg.in/yaml.v3"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
@@ -102,44 +99,3 @@ var _ = Describe(
 				"Trivy scan report contains High severity vulnerabilities")
 		})
 	})
-
-func prepareRapidastConfig(destinationPath string) error {
-
-	workingDir, err := os.Getwd()
-	Expect(err).ToNot(HaveOccurred(), err)
-
-	templateDir := filepath.Join(workingDir, rhwaparams.RapidastTemplateFolder)
-	rapidastConfigFile := filepath.Join(templateDir, rhwaparams.RapidastTemplateFile)
-
-	f, err := os.Open(rapidastConfigFile)
-	Expect(err).NotTo((HaveOccurred()))
-	defer f.Close()
-
-	content, err := io.ReadAll(f)
-	Expect(err).NotTo((HaveOccurred()))
-
-	yamlData := make(map[string]interface{})
-	err = yaml.Unmarshal(content, &yamlData)
-	Expect(err).NotTo((HaveOccurred()))
-
-	sc := yamlData["scanners"].(map[string]interface{})
-	genericTrivy := sc["generic_trivy"].(map[string]interface{})
-	genericTrivy["inline"] = fmt.Sprintf("trivy k8s --kubeconfig=/home/rapidast/.kube/config -n %s pod --severity=HIGH,CRITICAL --scanners=misconfig --report all --format json",
-		rhwaparams.RhwaOperatorNs)
-
-	modifiedYaml, err := yaml.Marshal(yamlData)
-	Expect(err).NotTo((HaveOccurred()))
-
-	filePath := filepath.Join(destinationPath, rhwaparams.RapidastTemplateFile)
-	rapidastConfig, err := os.Create(filePath)
-	Expect(err).NotTo((HaveOccurred()))
-	defer rapidastConfig.Close()
-
-	_, err = rapidastConfig.Write(modifiedYaml)
-	Expect(err).NotTo((HaveOccurred()))
-
-	err = rapidastConfig.Close()
-	Expect(err).NotTo((HaveOccurred()))
-
-	return nil
-}
