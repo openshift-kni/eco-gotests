283778439531a21fc99b3542ff1f9a7adc6c873c
Francisco Javier Moreno <frajamomo@hotmail.com>
2024-10-18 14:42:20 +0200

    Code refactoring for readiblity

--- tests/rhwa/internal/rhwaparams/const.go
+++ tests/rhwa/internal/rhwaparams/const.go
@@ -12,9 +12,9 @@ const (
 	// DefaultTimeout represents the default timeout.
 	DefaultTimeout = 300 * time.Second
 
-	// RhwaLogLevel represents the default log level for this suite.
 	RhwaLogLevel = 90
 
 	RapidastTemplateFolder = "../internal/dast/config-files"
 	RapidastTemplateFile   = "rapidastConfig.yaml"
+	RapidastImage          = "quay.io/redhatproductsecurity/rapidast:2.7.0"
 )
