01b3fa31a07af70d07570a4f611e6c238c16b769
Francisco Javier Moreno <frajamomo@hotmail.com>
2024-10-18 14:42:20 +0200

    Code refactoring for readiblity

--- tests/rhwa/far-operator/tests/far.go
+++ tests/rhwa/far-operator/tests/far.go
@@ -38,6 +38,7 @@ var _ = Describe(
 			By("Verify FAR deployment is Ready")
 			Expect(farDeployment.IsReady(rhwaparams.DefaultTimeout)).To(BeTrue(), "FAR deployment is not Ready")
 		})
+
 		It("Verify Fence Agents Remediation Operator pod is running", reportxml.ID("66026"), func() {
 
 			listOptions := metav1.ListOptions{
@@ -52,17 +53,13 @@ var _ = Describe(
 			Expect(err).ToNot(HaveOccurred(), "Pod is not ready")
 		})
 
-		It("Verify Fence Agents Remediation Operator passes trivy scan without vulnerabilities found", reportxml.ID("76877"), func() {
+		It("Verify Fence Agents Remediation Operator passes trivy scan without vulnerabilities", reportxml.ID("76877"), func() {
 
 			By("Creating temp directory")
 			dirname, err := os.MkdirTemp("", "case76877_*")
 			os.Chmod(dirname, 0755)
 			Expect(err).NotTo(HaveOccurred())
 			defer os.RemoveAll(dirname)
-
-			By("Creating rapidast configuration")
-			err = prepareRapidastConfig(dirname)
-			Expect(err).NotTo(HaveOccurred())
 
 			By("Creating rapidast output folder")
 			resultsDirname := fmt.Sprintf("%s/results", dirname)
@@ -73,9 +70,7 @@ var _ = Describe(
 			defer func() {
 				command := fmt.Sprintf("sudo chown $USER -R %s", resultsDirname)
 				_, permErrResultsDir := exec.Command("bash", "-c", command).Output()
-				if permErrResultsDir != nil {
-					glog.V(rhwaparams.RhwaLogLevel).Infof("Error %s occurred restoring of permission for results directory", resultsDirname)
-				}
+				Expect(permErrResultsDir).To(BeNil(), "Error occurred restoring of permission for results directory")
 			}()
 			command := fmt.Sprintf("podman unshare chown 1000 %s", resultsDirname)
 			_, permErrResultsDir := exec.Command("bash", "-c", command).Output()
@@ -85,11 +80,16 @@ var _ = Describe(
 			kubeconfigPath := os.Getenv("KUBECONFIG")
 			Expect(kubeconfigPath).NotTo(BeNil())
 
+			By("Creating rapidast configuration")
+			err = prepareRapidastConfig(dirname)
+			Expect(err).NotTo(HaveOccurred())
+
 			By("Creating podman command")
-			rapiDastCmd := fmt.Sprintf("podman run -it --rm -v %s:/home/rapidast/.kube/config:Z -v %s:/test:Z -v %s:/opt/rapidast/results:Z quay.io/redhatproductsecurity/rapidast:2.7.0 rapidast.py --config /test/%s",
+			rapiDastCmd := fmt.Sprintf("podman run -it --rm -v %s:/home/rapidast/.kube/config:Z -v %s:/test:Z -v %s:/opt/rapidast/results:Z %s rapidast.py --config /test/%s",
 				kubeconfigPath,
 				dirname,
 				resultsDirname,
+				rhwaparams.RapidastImage,
 				rhwaparams.RapidastTemplateFile,
 			)
 			By("Running podman command")
@@ -97,12 +97,9 @@ var _ = Describe(
 			Expect(err).NotTo(HaveOccurred(), "Error occured during execution of RapiDast test")
 			glog.V(rhwaparams.RhwaLogLevel).Infof("RapiDast test execution output is %s\n", rapiDastOutput)
 
-			// Check if vulnerabilities have been detected by trivy
-			if !strings.Contains(string(rapiDastOutput), `"Severity": "HIGH"`) {
-				glog.V(rhwaparams.RhwaLogLevel).Infof("Trivy scan has been executed successfully, no issues detected")
-			} else {
-				glog.V(rhwaparams.RhwaLogLevel).Infof("Check trivy scan result and see if any vulnerabilities has been detected")
-			}
+			By("Checking output of trivy scan")
+			Expect(strings.Contains(string(rapiDastOutput), `"Severity": "HIGH"`)).NotTo(BeTrue(),
+				"Trivy scan report contains High severity vulnerabilities")
 		})
 	})
 
@@ -127,7 +124,8 @@ func prepareRapidastConfig(destinationPath string) error {
 
 	sc := yamlData["scanners"].(map[string]interface{})
 	genericTrivy := sc["generic_trivy"].(map[string]interface{})
-	genericTrivy["inline"] = fmt.Sprintf("trivy k8s --kubeconfig=/home/rapidast/.kube/config -n %s pod --severity=HIGH,CRITICAL --scanners=misconfig --report all --format json", rhwaparams.RhwaOperatorNs)
+	genericTrivy["inline"] = fmt.Sprintf("trivy k8s --kubeconfig=/home/rapidast/.kube/config -n %s pod --severity=HIGH,CRITICAL --scanners=misconfig --report all --format json",
+		rhwaparams.RhwaOperatorNs)
 
 	modifiedYaml, err := yaml.Marshal(yamlData)
 	Expect(err).NotTo((HaveOccurred()))
