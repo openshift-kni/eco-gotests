8ae9b344b6fb6a47fe7cf92b53662b612de3dd09
Francisco Javier Moreno <frajamomo@hotmail.com>
2024-10-18 12:54:11 +0200

    Run trivy scan on FAR operator initial

--- tests/rhwa/internal/rhwaparams/const.go
+++ tests/rhwa/internal/rhwaparams/const.go
@@ -8,7 +8,13 @@ const (
 	// Label represents rhwa label that can be used for test cases selection.
 	Label = "rhwa"
 	// RhwaOperatorNs custom namespace of rhwa operators.
-	RhwaOperatorNs = "openshift-operators"
+	RhwaOperatorNs = "openshift-workload-availability"
 	// DefaultTimeout represents the default timeout.
 	DefaultTimeout = 300 * time.Second
+
+	// RhwaLogLevel represents the default log level for this suite.
+	RhwaLogLevel = 90
+
+	RapidastTemplateFolder = "../internal/dast/config-files"
+	RapidastTemplateFile   = "rapidastConfig.yaml"
 )
