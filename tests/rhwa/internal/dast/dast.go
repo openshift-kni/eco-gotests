734782e4ea223d78cd1385b3c0734d3978a12956
Francisco Javier Moreno <frajamomo@hotmail.com>
2024-10-24 10:57:45 +0200

    Fixes for lint errors

--- tests/rhwa/internal/dast/dast.go
+++ tests/rhwa/internal/dast/dast.go
@@ -6,48 +6,78 @@ import (
 	"os"
 	"path/filepath"
 
-	. "github.com/onsi/gomega"
 	"github.com/openshift-kni/eco-gotests/tests/rhwa/internal/rhwaparams"
 	"gopkg.in/yaml.v3"
 )
 
+// PrepareRapidastConfig builds the trivy command and updates the rapidast configuration file.
 func PrepareRapidastConfig(destinationPath string) error {
-
 	workingDir, err := os.Getwd()
-	Expect(err).ToNot(HaveOccurred(), err)
+	if err != nil {
+		return err
+	}
 
 	templateDir := filepath.Join(workingDir, rhwaparams.RapidastTemplateFolder)
 	rapidastConfigFile := filepath.Join(templateDir, rhwaparams.RapidastTemplateFile)
 
-	f, err := os.Open(rapidastConfigFile)
-	Expect(err).NotTo((HaveOccurred()))
-	defer f.Close()
+	file, err := os.Open(rapidastConfigFile)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
 
-	content, err := io.ReadAll(f)
-	Expect(err).NotTo((HaveOccurred()))
+	content, err := io.ReadAll(file)
+	if err != nil {
+		return err
+	}
 
 	yamlData := make(map[string]interface{})
-	err = yaml.Unmarshal(content, &yamlData)
-	Expect(err).NotTo((HaveOccurred()))
 
-	sc := yamlData["scanners"].(map[string]interface{})
-	genericTrivy := sc["generic_trivy"].(map[string]interface{})
-	genericTrivy["inline"] = fmt.Sprintf("trivy k8s --kubeconfig=/home/rapidast/.kube/config -n %s pod --severity=HIGH,CRITICAL --scanners=misconfig --report all --format json",
+	err = yaml.Unmarshal(content, &yamlData)
+	if err != nil {
+		return err
+	}
+
+	sc, successful := yamlData["scanners"].(map[string]interface{})
+	if sc == nil || !successful {
+		return fmt.Errorf("yamlData is missing key \"scanners\"")
+	}
+
+	genericTrivy, successful := sc["generic_trivy"].(map[string]interface{})
+	if genericTrivy == nil || !successful {
+		return fmt.Errorf("yamlData is missing key \"generic_trivy\"")
+	}
+
+	genericTrivy["inline"] = fmt.Sprintf("trivy k8s --kubeconfig=/home/rapidast/.kube/config "+
+		"-n %s pod "+
+		"--severity=HIGH,CRITICAL "+
+		"--scanners=misconfig "+
+		"--report all "+
+		"--format json",
 		rhwaparams.RhwaOperatorNs)
 
 	modifiedYaml, err := yaml.Marshal(yamlData)
-	Expect(err).NotTo((HaveOccurred()))
+	if err != nil {
+		return err
+	}
 
 	filePath := filepath.Join(destinationPath, rhwaparams.RapidastTemplateFile)
+
 	rapidastConfig, err := os.Create(filePath)
-	Expect(err).NotTo((HaveOccurred()))
+	if err != nil {
+		return err
+	}
 	defer rapidastConfig.Close()
 
 	_, err = rapidastConfig.Write(modifiedYaml)
-	Expect(err).NotTo((HaveOccurred()))
+	if err != nil {
+		return err
+	}
 
 	err = rapidastConfig.Close()
-	Expect(err).NotTo((HaveOccurred()))
+	if err != nil {
+		return err
+	}
 
 	return nil
 }
--- tests/rhwa/internal/rhwaparams/const.go
+++ tests/rhwa/internal/rhwaparams/const.go
@@ -12,15 +12,15 @@ const (
 	// DefaultTimeout represents the default timeout.
 	DefaultTimeout = 300 * time.Second
 
-	// RhwaLogLevel represents the log level used in Glog statements
+	// RhwaLogLevel represents the log level used in Glog statements.
 	RhwaLogLevel = 90
 
-	// RapidastTemplateFolder represents the relative route of the folder storing the needed templates
+	// RapidastTemplateFolder represents the relative route of the folder storing the needed templates.
 	RapidastTemplateFolder = "../internal/dast/config-files"
 
-	// RapidastTemplateFile represents the file name used for rapiDast configuration
+	// RapidastTemplateFile represents the file name used for rapiDast configuration.
 	RapidastTemplateFile = "rapidastConfig.yaml"
 
-	// RapidastImage represents the rapidast container image used for dast testing
+	// RapidastImage represents the rapidast container image used for dast testing.
 	RapidastImage = "quay.io/redhatproductsecurity/rapidast:2.7.0"
 )
