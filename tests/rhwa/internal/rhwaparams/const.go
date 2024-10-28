41a837108217bebc862c4886c5667a7404de5224
Francisco Javier Moreno <frajamomo@hotmail.com>
2024-10-28 17:31:29 +0100

    rhwa: Fix default NS for rhwa operators (#265)

--- tests/rhwa/internal/rhwaparams/const.go
+++ tests/rhwa/internal/rhwaparams/const.go
@@ -8,7 +8,7 @@ const (
 	// Label represents rhwa label that can be used for test cases selection.
 	Label = "rhwa"
 	// RhwaOperatorNs custom namespace of rhwa operators.
-	RhwaOperatorNs = "openshift-operators"
+	RhwaOperatorNs = "openshift-workload-availability"
 	// DefaultTimeout represents the default timeout.
 	DefaultTimeout = 300 * time.Second
 )
