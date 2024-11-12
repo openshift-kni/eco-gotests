36b52e43dfdfc1ad52b61612e73f266c8764d12a
Francisco Javier Moreno <frajamomo@hotmail.com>
2024-11-12 16:27:09 +0100

    rhwa: DAST test on FAR operator (#261)
    * rhwa: DAST test on FAR operator
    
    Adds a test that runs rapidast scan for the FAR operator installed in the cluster.
    
    It configures the rapidast for the execution of the trivy scan, looking for
    misconfigurations on the pods running on the namespace where the operator is installed.
    
    The test fails if any High severity vulnerability is found, providing in the
    console output the details of the detection.
    
    * Code refactoring for readiblity
    
    * extract prepareRapidastConfig to separate file
    
    * fix import of dast related functions
    
    * Add comments to constants
    
    * Fixes for lint errors
    
    * Remove redundant file closure

--- tests/rhwa/internal/rhwaparams/const.go
+++ tests/rhwa/internal/rhwaparams/const.go
@@ -11,4 +11,16 @@ const (
 	RhwaOperatorNs = "openshift-workload-availability"
 	// DefaultTimeout represents the default timeout.
 	DefaultTimeout = 300 * time.Second
+
+	// RhwaLogLevel represents the log level used in Glog statements.
+	RhwaLogLevel = 90
+
+	// RapidastTemplateFolder represents the relative route of the folder storing the needed templates.
+	RapidastTemplateFolder = "../internal/dast/config-files"
+
+	// RapidastTemplateFile represents the file name used for rapiDast configuration.
+	RapidastTemplateFile = "rapidastConfig.yaml"
+
+	// RapidastImage represents the rapidast container image used for dast testing.
+	RapidastImage = "quay.io/redhatproductsecurity/rapidast:2.7.0"
 )
