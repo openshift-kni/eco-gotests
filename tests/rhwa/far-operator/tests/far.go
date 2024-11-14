d645e068c969ee33c81dcb567ff2c68f7069618d
Trey West <treywest45th@gmail.com>
2024-11-14 09:34:59 -0500

    Revert "rhwa: DAST test on FAR operator (#261)"
    This reverts commit 36b52e43dfdfc1ad52b61612e73f266c8764d12a.

--- tests/rhwa/far-operator/tests/far.go
+++ tests/rhwa/far-operator/tests/far.go
@@ -2,9 +2,6 @@ package tests
 
 import (
 	"fmt"
-	"os"
-	"os/exec"
-	"strings"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
@@ -14,11 +11,9 @@ import (
 	"github.com/openshift-kni/eco-goinfra/pkg/reportxml"
 
 	"github.com/openshift-kni/eco-gotests/tests/rhwa/far-operator/internal/farparams"
-	"github.com/openshift-kni/eco-gotests/tests/rhwa/internal/dast"
 	. "github.com/openshift-kni/eco-gotests/tests/rhwa/internal/rhwainittools"
 	"github.com/openshift-kni/eco-gotests/tests/rhwa/internal/rhwaparams"
 
-	"github.com/golang/glog"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
@@ -36,7 +31,6 @@ var _ = Describe(
 			By("Verify FAR deployment is Ready")
 			Expect(farDeployment.IsReady(rhwaparams.DefaultTimeout)).To(BeTrue(), "FAR deployment is not Ready")
 		})
-
 		It("Verify Fence Agents Remediation Operator pod is running", reportxml.ID("66026"), func() {
 
 			listOptions := metav1.ListOptions{
@@ -49,59 +43,5 @@ var _ = Describe(
 				listOptions,
 			)
 			Expect(err).ToNot(HaveOccurred(), "Pod is not ready")
-		})
-
-		It("Verify FAR Operator passes trivy scan without vulnerabilities", reportxml.ID("76877"), func() {
-
-			By("Creating temp directory")
-			dirname, err := os.MkdirTemp("", "case76877_*")
-			Expect(err).NotTo(HaveOccurred())
-			err = os.Chmod(dirname, 0755)
-			Expect(err).NotTo(HaveOccurred())
-			defer os.RemoveAll(dirname)
-
-			By("Creating rapidast output folder")
-			resultsDirname := fmt.Sprintf("%s/results", dirname)
-			err = os.MkdirAll(resultsDirname, 0755)
-			Expect(err).NotTo(HaveOccurred())
-			defer os.RemoveAll(resultsDirname)
-
-			defer func() {
-				command := fmt.Sprintf("sudo chown $USER -R %s", resultsDirname)
-				_, permErrResultsDir := exec.Command("bash", "-c", command).Output()
-				Expect(permErrResultsDir).To(BeNil(), "Error occurred restoring of permission for results directory")
-			}()
-			command := fmt.Sprintf("podman unshare chown 1000 %s", resultsDirname)
-			_, permErrResultsDir := exec.Command("bash", "-c", command).Output()
-			Expect(permErrResultsDir).To(BeNil(), "Error occurred updating permission for results directory")
-
-			By("Getting KUBECONFIG env variable for trivy")
-			kubeconfigPath := os.Getenv("KUBECONFIG")
-			Expect(kubeconfigPath).NotTo(BeNil())
-
-			By("Creating rapidast configuration")
-			err = dast.PrepareRapidastConfig(dirname)
-			Expect(err).NotTo(HaveOccurred())
-
-			By("Creating podman command")
-			rapiDastCmd := fmt.Sprintf("podman run -it --rm "+
-				"-v %s:/home/rapidast/.kube/config:Z "+
-				"-v %s:/test:Z "+
-				"-v %s:/opt/rapidast/results:Z "+
-				"%s rapidast.py --config /test/%s",
-				kubeconfigPath,
-				dirname,
-				resultsDirname,
-				rhwaparams.RapidastImage,
-				rhwaparams.RapidastTemplateFile,
-			)
-			By("Running podman command")
-			rapiDastOutput, err := exec.Command("bash", "-c", rapiDastCmd).Output()
-			Expect(err).NotTo(HaveOccurred(), "Error occurred during execution of RapiDast test")
-			glog.V(rhwaparams.RhwaLogLevel).Infof("RapiDast test execution output is %s\n", rapiDastOutput)
-
-			By("Checking output of trivy scan")
-			Expect(strings.Contains(string(rapiDastOutput), `"Severity": "HIGH"`)).NotTo(BeTrue(),
-				"Trivy scan report contains High severity vulnerabilities")
 		})
 	})
