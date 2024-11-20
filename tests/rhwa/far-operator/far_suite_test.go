c5bbc85775de5ddaabb2b35ad96cbcda09809bae
Francisco Javier Moreno <frajamomo@hotmail.com>
2024-11-20 18:30:52 +0100

    rhwa: Run FAR operator dast scan from pod

--- tests/rhwa/far-operator/far_suite_test.go
+++ tests/rhwa/far-operator/far_suite_test.go
@@ -6,14 +6,20 @@ import (
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	"github.com/openshift-kni/eco-goinfra/pkg/namespace"
 	"github.com/openshift-kni/eco-goinfra/pkg/reportxml"
+	"github.com/openshift-kni/eco-gotests/tests/internal/params"
 	"github.com/openshift-kni/eco-gotests/tests/internal/reporter"
 	"github.com/openshift-kni/eco-gotests/tests/rhwa/far-operator/internal/farparams"
 	_ "github.com/openshift-kni/eco-gotests/tests/rhwa/far-operator/tests"
 	. "github.com/openshift-kni/eco-gotests/tests/rhwa/internal/rhwainittools"
+	"github.com/openshift-kni/eco-gotests/tests/rhwa/internal/rhwaparams"
 )
 
-var _, currentFile, _, _ = runtime.Caller(0)
+var (
+	_, currentFile, _, _ = runtime.Caller(0)
+	testNS               = namespace.NewBuilder(APIClient, rhwaparams.TestNamespaceName)
+)
 
 func TestFAR(t *testing.T) {
 	_, reporterConfig := GinkgoConfiguration()
@@ -22,6 +28,15 @@ func TestFAR(t *testing.T) {
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "FAR", Label(farparams.Labels...), reporterConfig)
 }
+
+var _ = BeforeSuite(func() {
+	By("Creating test namespace with privileged labels")
+	for key, value := range params.PrivilegedNSLabels {
+		testNS.WithLabel(key, value)
+	}
+	_, err := testNS.Create()
+	Expect(err).ToNot(HaveOccurred(), "error to create test namespace")
+})
 
 var _ = JustAfterEach(func() {
 	reporter.ReportIfFailed(
@@ -32,3 +47,9 @@ var _ = ReportAfterSuite("", func(report Report) {
 	reportxml.Create(
 		report, RHWAConfig.GetReportPath(), RHWAConfig.TCPrefix)
 })
+
+//var _ = AfterSuite(func() {
+//	By("Deleting test namespace")
+//	err := testNS.DeleteAndWait(rhwaparams.DefaultTimeout)
+//	Expect(err).ToNot(HaveOccurred(), "error to delete test namespace")
+//})
