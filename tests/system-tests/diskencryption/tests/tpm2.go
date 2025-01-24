94d8292290f714f3b4265b0128d82b5a3e8364c8
David Elie-Dit-Cosaque <deliedit@redhat.com>
2025-01-24 10:41:09 -0600

    save original boot order instead of reading current value and swapping when recovering

--- tests/system-tests/diskencryption/tests/tpm2.go
+++ tests/system-tests/diskencryption/tests/tpm2.go
@@ -413,6 +413,22 @@ var _ = Describe("TPM2", func() {
 		isRootDiskReservedSlotPresent := helper.LuksListContainsReservedSlot(luksListOutput)
 		Expect(isRootDiskReservedSlotPresent).To(BeFalse(), "there should be no reserved slot present at this point")
 
+		By("saving the original server boot order")
+		var originalBootOrder []string
+		Eventually(func() error {
+			originalBootOrder, err = BMCClient.SystemBootOrderReferences()
+
+			if len(originalBootOrder) < 2 {
+				return fmt.Errorf("need at least 2 boot entries")
+			}
+
+			return err
+		}).WithTimeout(tsparams.TimeoutWaitingOnBMC).
+			WithPolling(tsparams.PollingIntervalBMC).
+			ShouldNot(HaveOccurred(), "getting boot order should not return an error")
+
+		By(fmt.Sprintf("listing original boot Order: %s", originalBootOrder))
+
 		By("changing the server boot order")
 		swapFirstSecondBootItems()
 
@@ -439,8 +455,10 @@ var _ = Describe("TPM2", func() {
 		Expect(err).ToNot(HaveOccurred(), "WaitForRegex should not fail")
 		Expect(matchIndex).To(Equal(0), "WaitForRegex should match TPM failed (0)")
 
-		By("changing the server boot order back to defaults")
-		swapFirstSecondBootItems()
+		By("changing the server boot order back to original settings")
+		Eventually(BMCClient.SetSystemBootOrderReferences).WithTimeout(tsparams.TimeoutWaitingOnBMC).
+			WithPolling(tsparams.PollingIntervalBMC).WithArguments(originalBootOrder).ShouldNot(HaveOccurred(),
+			"changing boot order should not return an error")
 
 		By("power cycling node")
 		Eventually(BMCClient.SystemPowerCycle).WithTimeout(tsparams.TimeoutWaitingOnBMC).
