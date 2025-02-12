fcb5b64d8ce24a1b0af265dfb377b65bd25d7511
Gregory Kopels <gkopels@gkopels-thinkpadx1carbongen9.raanana.csb>
2025-02-12 14:38:05 +0200

    cnf network: add two custom firewall test cases

--- tests/cnf/core/network/security/internal/tsparams/consts.go
+++ tests/cnf/core/network/security/internal/tsparams/consts.go
@@ -20,4 +20,35 @@ table inet custom_table {
 		tcp dport 8888 log prefix "[USERFIREWALL] PACKET DROP: " drop
 	}
 }`
+
+	// CustomFirewallIngressEgressPort8888 creates a custom firewall table blocking incoming and outgoing tcp port 8888.
+	CustomFirewallIngressEgressPort8888 = `table inet custom_table
+delete table inet custom_table
+table inet custom_table {
+	chain custom_chain_INPUT {
+		type filter hook input priority 1; policy accept;
+		# Drop TCP port 8888 and log
+		tcp dport 8888 log prefix "[USERFIREWALL] PACKET DROP: " drop
+	}
+	chain custom_chain_OUTPUT {
+		type filter hook output priority 1; policy accept;
+		# Drop TCP port 8888 and log
+		tcp dport 8888 log prefix "[USERFIREWALL] PACKET DROP: " drop
+}`
+
+	// CustomFirewallIngress8888EgressPort8088 creates a custom firewall table blocking ingress
+	// port 8888 and egress port 8088.
+	CustomFirewallIngress8888EgressPort8088 = `table inet custom_table
+delete table inet custom_table
+table inet custom_table {
+	chain custom_chain_INPUT {
+		type filter hook input priority 1; policy accept;
+		# Drop TCP port 8888 and log
+		tcp dport 8888 log prefix "[USERFIREWALL] PACKET DROP: " drop
+	}
+	chain custom_chain_OUTPUT {
+		type filter hook output priority 1; policy accept;
+		# Drop TCP port 8088 and log
+		tcp dport 8088 log prefix "[USERFIREWALL] PACKET DROP: " drop
+}`
 )
