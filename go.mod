da2b6cde91815d637e7c72c75b80dd3931cc25ea
Dwaine Gonyier <dgonyier@redhat.com>
2025-04-17 19:21:39 -0400

    Initial Commit for ZTP deployment type tests

--- go.mod
+++ go.mod
@@ -249,6 +249,26 @@ require (
 	sigs.k8s.io/yaml v1.4.0 // indirect
 )
 
+require github.com/go-git/go-git/v5 v5.12.0
+
+require (
+	dario.cat/mergo v1.0.1 // indirect
+	github.com/Microsoft/go-winio v0.6.2 // indirect
+	github.com/ProtonMail/go-crypto v1.0.0 // indirect
+	github.com/cloudflare/circl v1.3.7 // indirect
+	github.com/cyphar/filepath-securejoin v0.2.5 // indirect
+	github.com/emirpasic/gods v1.18.1 // indirect
+	github.com/go-git/gcfg v1.5.1-0.20230307220236-3a3c6141e376 // indirect
+	github.com/go-git/go-billy/v5 v5.5.0 // indirect
+	github.com/jbenet/go-context v0.0.0-20150711004518-d14ea06fba99 // indirect
+	github.com/kevinburke/ssh_config v1.2.0 // indirect
+	github.com/pjbgf/sha1cd v0.3.0 // indirect
+	github.com/sergi/go-diff v1.3.2-0.20230802210424-5b0b94c5c0d3 // indirect
+	github.com/skeema/knownhosts v1.2.2 // indirect
+	github.com/xanzy/ssh-agent v0.3.3 // indirect
+	gopkg.in/warnings.v0 v0.1.2 // indirect
+)
+
 replace (
 	github.com/imdario/mergo => github.com/imdario/mergo v0.3.16
 	github.com/k8snetworkplumbingwg/sriov-network-operator => github.com/openshift/sriov-network-operator v0.0.0-20250102160645-f496851fbd0d // release-4.18
---
+++ tests/cnf/ran-deployment/internal/ranparam/const.go
@@ -0,0 +1,42 @@
+package ranparam
+
+import (
+	"github.com/golang/glog"
+)
+
+const (
+	// Label represents the label for the ran test cases.
+	Label string = "ran-deployment"
+
+	// AcmOperatorNamespace ACM's namespace.
+	AcmOperatorNamespace string = "rhacm"
+	// MceOperatorNamespace is the namespace for the MCE operator.
+	MceOperatorNamespace string = "multicluster-engine"
+	// TalmOperatorHubNamespace TALM namespace.
+	TalmOperatorHubNamespace string = "topology-aware-lifecycle-manager"
+	// TalmContainerName is the name of the container in the talm pod.
+	TalmContainerName string = "manager"
+	// OpenshiftOperatorNamespace is the namespace where operators are.
+	OpenshiftOperatorNamespace string = "openshift-operators"
+	// OpenshiftGitOpsNamespace is the namespace for the GitOps operator.
+	OpenshiftGitOpsNamespace string = "openshift-gitops"
+	// OpenshiftGitopsRepoServer ocp git repo server.
+	OpenshiftGitopsRepoServer string = "openshift-gitops-repo-server"
+
+	// LogLevel is the verbosity for ran/internal packages.
+	LogLevel glog.Level = 80
+)
+
+// HubOperatorName represets the possible operator names that may have associated versions on the hub cluster.
+type HubOperatorName string
+
+const (
+	// ACM is the name of the advanced cluster management operator.
+	ACM HubOperatorName = "advanced-cluster-management"
+	// TALM is the name of the topology aware lifecycle manager operator.
+	TALM HubOperatorName = "topology-aware-lifecycle-manager"
+	// GitOps is the name of the GitOps operator.
+	GitOps HubOperatorName = "openshift-gitops-operator"
+	// MCE is the name of the multicluster engine operator.
+	MCE HubOperatorName = "multicluster-engine"
+)
