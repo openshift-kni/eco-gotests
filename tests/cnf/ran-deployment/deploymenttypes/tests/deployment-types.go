60e30d0165e0fc303e89f10bfb3258909816b54a
Dwaine Gonyier <dgonyier@redhat.com>
2025-04-17 19:21:39 -0400

    Initial Commit for ZTP deployment type tests

---
+++ tests/cnf/ran-deployment/deploymenttypes/tests/deployment-types.go
@@ -0,0 +1,453 @@
+package tests
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"regexp"
+	"strings"
+	"time"
+
+	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing"
+	"github.com/go-git/go-git/v5/plumbing/object"
+	"github.com/golang/glog"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"github.com/openshift-kni/eco-goinfra/pkg/argocd"
+	"github.com/openshift-kni/eco-goinfra/pkg/clients"
+	"github.com/openshift-kni/eco-goinfra/pkg/hive"
+	"github.com/openshift-kni/eco-goinfra/pkg/nodes"
+	"github.com/openshift-kni/eco-goinfra/pkg/ocm"
+	"github.com/openshift-kni/eco-goinfra/pkg/reportxml"
+	"github.com/openshift-kni/eco-gotests/tests/cnf/ran-deployment/deploymenttypes/internal/gitdetails"
+	"github.com/openshift-kni/eco-gotests/tests/cnf/ran-deployment/deploymenttypes/internal/tsparams"
+	. "github.com/openshift-kni/eco-gotests/tests/cnf/ran-deployment/internal/raninittools"
+	"github.com/openshift-kni/eco-gotests/tests/cnf/ran-deployment/internal/ranparam"
+	"gopkg.in/yaml.v3"
+	policiesv1 "open-cluster-management.io/governance-policy-propagator/api/v1"
+	runtimeclient "sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+const (
+	gitSiteConfigCloneDir      string = "ztp-deployment-siteconfig"
+	gitPolicyTemplatesCloneDir string = "ztp-deployment-policy-templates"
+)
+
+var (
+	reHubSideTemplate           = regexp.MustCompile(`\{\{\s*hub[^\r\n]+hub\s*\}\}`)
+	ignorePaths       [5]string = [5]string{
+		"source-crs/",
+		"custom-crs/",
+		"extra-manifest/",
+		"extra-manifests/",
+		"ztp-test/",
+	}
+)
+
+var _ = Describe("Cluster Deployment Types Tests", Ordered, Label(tsparams.LabelDeploymentTypeTestCases), func() {
+	var (
+		siteconfigRepo *git.Repository
+		policiesRepo   *git.Repository
+		pathSiteConfig string
+		pathPolicies   string
+
+		deploymentMethod tsparams.DeploymentType
+		policyTemplate   tsparams.PolicyType
+		isMultiCluster   tsparams.MultiClusterType
+		clusterKind      tsparams.ClusterType
+
+		policiesApp *argocd.ApplicationBuilder
+		clustersApp *argocd.ApplicationBuilder
+	)
+
+	BeforeAll(func() {
+		// Determine if cluster deployments were successful, check for compliant policies for each cluster
+		Expect(HubAPIClient).ToNot(BeNil(), "HubAPIClient is nil")
+		Expect(HubAPIClient.KubeconfigPath).ToNot(BeEmpty(), "KUBECONFIG for hub cluster is not provided.")
+
+		Expect(Spoke1APIClient).ToNot(BeNil(), "Spoke1APIClient is nil")
+		Expect(Spoke1APIClient.KubeconfigPath).ToNot(BeEmpty(), "KUBECONFIG for first cluster is not provided.")
+
+		err := ocm.WaitForAllPoliciesComplianceState(
+			HubAPIClient, policiesv1.Compliant, time.Minute, runtimeclient.ListOptions{Namespace: RANConfig.Spoke1Name})
+
+		Expect(err).ToNot(HaveOccurred(), "Failed to verify all policies are compliant for spoke %s", RANConfig.Spoke1Name)
+
+		clusterKind = getClusterType(Spoke1APIClient, RANConfig.Spoke1Name)
+
+		if Spoke2APIClient != nil && Spoke2APIClient.KubeconfigPath != "" {
+			err = ocm.WaitForAllPoliciesComplianceState(
+				HubAPIClient, policiesv1.Compliant, time.Minute, runtimeclient.ListOptions{Namespace: RANConfig.Spoke2Name})
+
+			Expect(err).ToNot(HaveOccurred(), "Failed to verify all policies are compliant for spoke %s", RANConfig.Spoke2Name)
+
+			isMultiCluster = tsparams.MultiCluster
+
+		} else {
+			glog.V(tsparams.LogLevel).Infof("Second cluster is not available")
+			isMultiCluster = tsparams.SingleCluster
+		}
+
+		policiesApp, err = argocd.PullApplication(
+			HubAPIClient, tsparams.ArgoCdPoliciesAppName, ranparam.OpenshiftGitOpsNamespace)
+		Expect(err).ToNot(HaveOccurred(), "Failed to get the policies app")
+
+		policiesSource, err := gitdetails.GetGitSource(policiesApp)
+		Expect(err).ToNot(HaveOccurred(), "Failed to get the policies app git source")
+
+		pathPolicies = policiesSource.Path
+
+		clustersApp, err = argocd.PullApplication(
+			HubAPIClient, tsparams.ArgoCdClustersAppName, ranparam.OpenshiftGitOpsNamespace)
+		Expect(err).ToNot(HaveOccurred(), "Failed to get the clusters app")
+
+		clustersSource, err := gitdetails.GetGitSource(clustersApp)
+		Expect(err).ToNot(HaveOccurred(), "Failed to get the clusters app git source")
+
+		pathSiteConfig = clustersSource.Path
+
+		glog.V(tsparams.LogLevel).Infof("Successful retreival of apps git details")
+
+		mkGitCloneDirs()
+
+		siteconfigRepo, policiesRepo = gitCloneToDirs(clustersApp, policiesApp)
+
+		// Examine files in repos
+		deploymentMethod, _ = getFilesInfo(siteconfigRepo, pathSiteConfig)
+		_, policyTemplate = getFilesInfo(policiesRepo, pathPolicies)
+
+		// Check for siteconfig deployment type
+		deploymentMethod = getDeploymentMethod(HubAPIClient, RANConfig.Spoke1Name, deploymentMethod)
+	})
+
+	AfterAll(func() {
+
+		// Clean up git clone directories
+		rmGitCloneDirs()
+	})
+
+	It(fmt.Sprintf("checks if deployment is %s", tsparams.MultiCluster), reportxml.ID("80498"), func() {
+
+		Expect(isMultiCluster == tsparams.SingleCluster || isMultiCluster == tsparams.MultiCluster).To(BeTrueBecause(
+			"Deployment must either be single cluster or multi cluster"))
+
+		if isMultiCluster == tsparams.SingleCluster {
+			Skip(fmt.Sprintf("Not %s deployment", tsparams.MultiCluster))
+		}
+
+		glog.V(tsparams.LogLevel).Info("Deployment is multi cluster")
+	})
+
+	DescribeTable("checks deployment method",
+		func(methodValue *tsparams.DeploymentType, kindValue tsparams.DeploymentType) {
+
+			Expect(*methodValue).ToNot(BeEmpty(), "deployMethod should not be empty")
+
+			if *methodValue != kindValue {
+				Skip(fmt.Sprintf("Not %s install method", kindValue))
+			}
+
+			glog.V(tsparams.LogLevel).Infof("Install method is %s", kindValue)
+		},
+		func(methodValue *tsparams.DeploymentType, kindValue tsparams.DeploymentType) string {
+			return fmt.Sprintf("checks if deployment method is %s", kindValue)
+		},
+		Entry(nil, &deploymentMethod, tsparams.DeploymentImageBasedCI, reportxml.ID("80495")),
+		Entry(nil, &deploymentMethod, tsparams.DeploymentAssistedCI, reportxml.ID("80494")),
+		Entry(nil, &deploymentMethod, tsparams.DeploymentSiteConfig, reportxml.ID("80493")),
+	)
+
+	DescribeTable("checks policy kind",
+		func(policyValue *tsparams.PolicyType, kindValue tsparams.PolicyType) {
+
+			Expect(*policyValue).ToNot(BeEmpty(), "policyTemplate should not be empty")
+
+			if *policyValue != kindValue {
+				Skip(fmt.Sprintf("Not %s policy type", kindValue))
+			}
+
+			glog.V(tsparams.LogLevel).Infof("Policy type is %s", kindValue)
+		},
+		func(policyValue *tsparams.PolicyType, kindValue tsparams.PolicyType) string {
+			return fmt.Sprintf("checks if policy type is %s", kindValue)
+		},
+		Entry(nil, &policyTemplate, tsparams.PolicyPGT, reportxml.ID("80496")),
+		Entry(nil, &policyTemplate, tsparams.PolicyACMPG, reportxml.ID("80502")),
+		Entry(nil, &policyTemplate, tsparams.PolicyPGTHST, reportxml.ID("80501")),
+		Entry(nil, &policyTemplate, tsparams.PolicyACMPGHST, reportxml.ID("80503")),
+	)
+
+	DescribeTable("checks cluster type",
+		func(clusterValue *tsparams.ClusterType, kindValue tsparams.ClusterType) {
+
+			Expect(*clusterValue).ToNot(BeEmpty(), "clusterKind should not be empty")
+
+			if *clusterValue != kindValue {
+				Skip(fmt.Sprintf("Not %s cluster type", kindValue))
+			}
+
+			glog.V(tsparams.LogLevel).Infof("Cluster type is %s", kindValue)
+		},
+		func(clusterValue *tsparams.ClusterType, kindValue tsparams.ClusterType) string {
+			return fmt.Sprintf("checks if cluster type is %s", kindValue)
+		},
+		Entry(nil, &clusterKind, tsparams.ClusterSNO, reportxml.ID("80497")),
+		Entry(nil, &clusterKind, tsparams.ClusterSNOPlusWorker, reportxml.ID("81679")),
+		Entry(nil, &clusterKind, tsparams.ClusterThreeNode, reportxml.ID("80499")),
+		Entry(nil, &clusterKind, tsparams.ClusterStandard, reportxml.ID("80500")),
+	)
+
+})
+
+// Clean up git clone dirs if they exist and create empty dirctories for git clone targets.
+func mkGitCloneDirs() {
+	rmGitCloneDirs()
+
+	err := os.MkdirAll(gitSiteConfigCloneDir, 0755)
+	Expect(err).ToNot(HaveOccurred(), "Failed to create %s directory", gitSiteConfigCloneDir)
+
+	err = os.MkdirAll(gitPolicyTemplatesCloneDir, 0755)
+	Expect(err).ToNot(HaveOccurred(), "Failed to create %s directory", gitPolicyTemplatesCloneDir)
+}
+
+// Delete git clone directories.
+func rmGitCloneDirs() {
+	err := os.RemoveAll(gitSiteConfigCloneDir)
+	Expect(err).ToNot(HaveOccurred(), "Failed to remove %s directory", gitSiteConfigCloneDir)
+
+	err = os.RemoveAll(gitPolicyTemplatesCloneDir)
+	Expect(err).ToNot(HaveOccurred(), "Failed to remove %s directory", gitPolicyTemplatesCloneDir)
+}
+
+// git clone siteconfig and policy templates to target directories.
+// clusters and policies apps are cloned separately to allow for
+// the case where they point to different repos/branches/paths.
+func gitCloneToDirs(clustersApp,
+	policiesApp *argocd.ApplicationBuilder) (
+	siteconfigRepo, policiesRepo *git.Repository) {
+	clustersSource, err := gitdetails.GetGitSource(clustersApp)
+	Expect(err).ToNot(HaveOccurred(), "Failed to get clusters app git source details")
+
+	policiesSource, err := gitdetails.GetGitSource(policiesApp)
+	Expect(err).ToNot(HaveOccurred(), "Failed to get policies app git source details")
+
+	siteconfigRepo, err = git.PlainClone(gitSiteConfigCloneDir, false, &git.CloneOptions{
+		URL:           clustersSource.RepoURL,
+		Tags:          git.NoTags,
+		ReferenceName: plumbing.ReferenceName(clustersSource.TargetRevision),
+		Depth:         1,
+		SingleBranch:  true,
+		Progress:      nil,
+	})
+	Expect(err).ToNot(HaveOccurred(), "Failed to git clone siteconfig repo %s branch %s to directory %s",
+		clustersSource.RepoURL, clustersSource.TargetRevision, gitSiteConfigCloneDir)
+	glog.V(tsparams.LogLevel).Infof("Successful git clone of sitconfig repo %s branch %s",
+		clustersSource.RepoURL, clustersSource.TargetRevision)
+	glog.V(tsparams.LogLevel).Infof("Path in worktree: %s", clustersSource.Path)
+
+	policiesRepo, err = git.PlainClone(gitPolicyTemplatesCloneDir, false, &git.CloneOptions{
+		URL:           policiesSource.RepoURL,
+		Tags:          git.NoTags,
+		ReferenceName: plumbing.ReferenceName(policiesSource.TargetRevision),
+		Depth:         1,
+		SingleBranch:  true,
+		Progress:      nil,
+	})
+	Expect(err).ToNot(HaveOccurred(), "Failed to git clone policies repo %s branch %s to directory %s",
+		policiesSource.RepoURL, policiesSource.TargetRevision, gitPolicyTemplatesCloneDir)
+	glog.V(tsparams.LogLevel).Infof("Successful git clone of policies repo %s branch %s",
+		policiesSource.RepoURL, policiesSource.TargetRevision)
+	glog.V(tsparams.LogLevel).Infof("Path in worktree: %s", policiesSource.Path)
+
+	return siteconfigRepo, policiesRepo
+}
+
+// Get information from the files in the repo, filtering files by extensions, path, and "kind".
+func getFilesInfo(repo *git.Repository, path string) (tsparams.DeploymentType, tsparams.PolicyType) {
+	var (
+		deploymentMethod tsparams.DeploymentType = ""
+		policyTemplate   tsparams.PolicyType     = ""
+	)
+
+	remotes, err := repo.Remotes()
+
+	Expect(err).ToNot(HaveOccurred(), "Failed to get list of remotes")
+	glog.V(tsparams.LogLevel).Infof("Remote: %s", remotes[0].Config().URLs[0])
+
+	head, err := repo.Head()
+	Expect(err).ToNot(HaveOccurred(), "Failed to get branch head")
+
+	commit, err := repo.CommitObject(head.Hash())
+	Expect(err).ToNot(HaveOccurred(), "Failed to get commit")
+
+	tree, err := commit.Tree()
+	Expect(err).ToNot(HaveOccurred(), "Failed to get file tree")
+
+	subtree, err := tree.Tree(path)
+	Expect(err).ToNot(HaveOccurred(), "Failed to get file subtree for path %s", path)
+
+	err = subtree.Files().ForEach(func(fileEntry *object.File) error {
+		for _, ignorePath := range ignorePaths {
+			if strings.Contains(fileEntry.Name, ignorePath) {
+				glog.V(tsparams.LogLevel).Infof("Skipping reference or test CR file: %s", fileEntry.Name)
+
+				return nil
+			}
+		}
+
+		if filepath.Ext(fileEntry.Name) == ".yaml" || filepath.Ext(fileEntry.Name) == ".yml" {
+			glog.V(tsparams.LogLevel).Infof("Path: %s", fileEntry.Name)
+
+			content, err := fileEntry.Contents()
+			Expect(err).ToNot(HaveOccurred(), "Failed to get file content")
+
+			contentBytes := []byte(content)
+
+			// Get YAML Kind value.
+			kind := getYAMLKind(contentBytes, fileEntry.Name)
+
+			glog.V(tsparams.LogLevel).Infof("Kind from YAML: %s", kind)
+
+			// Determine deployment and policy types
+			switch kind {
+			case string(tsparams.DeploymentSiteConfig):
+				deploymentMethod = tsparams.DeploymentSiteConfig
+			case string(tsparams.PolicyPGT):
+				hasHST := checkForHubSideTemplate(contentBytes)
+
+				if !hasHST && policyTemplate != tsparams.PolicyPGTHST {
+					policyTemplate = tsparams.PolicyPGT
+				} else if hasHST {
+					policyTemplate = tsparams.PolicyPGTHST
+				}
+			case string(tsparams.PolicyACMPG):
+				hasHST := checkForHubSideTemplate(contentBytes)
+
+				if !hasHST && policyTemplate != tsparams.PolicyACMPGHST {
+					policyTemplate = tsparams.PolicyACMPG
+				} else if hasHST {
+					policyTemplate = tsparams.PolicyACMPGHST
+				}
+			}
+
+			return nil
+		}
+
+		glog.V(tsparams.LogLevel).Infof("Skipping non-YAML file: %s", fileEntry.Name)
+
+		return nil
+	})
+	Expect(err).ToNot(HaveOccurred(), "Failed to get file iterator")
+
+	return deploymentMethod, policyTemplate
+}
+
+// unmarshal YAML and get CR kind. Return empty string if kind is not found in YAML.
+func getYAMLKind(fileData []byte, fileName string) string {
+	fileContent := make(map[string]any)
+	err := yaml.Unmarshal(fileData, &fileContent)
+	Expect(err).ToNot(HaveOccurred(), "Failed to unmarshal file %s as yaml", fileName)
+
+	kind, result := fileContent["kind"].(string)
+	if !result {
+		glog.V(tsparams.LogLevel).Infof("Failed to determine kind from file %s", fileName)
+
+		return ""
+	}
+
+	return kind
+}
+
+// Check file for hub-side templating syntax.
+func checkForHubSideTemplate(content []byte) bool {
+	return reHubSideTemplate.Match(content)
+}
+
+// getCluterType determines the cluster type as one of: standard, 3node, SNO, SNO+Worker.
+func getClusterType(cluster *clients.Settings, clusterName string) tsparams.ClusterType {
+	var (
+		workerCount                            = 0
+		controlPlaneCount                      = 0
+		clusterKind       tsparams.ClusterType = ""
+	)
+
+	if cluster.KubeconfigPath == "" {
+		glog.V(tsparams.LogLevel).Infof("Cluster %s KUBECONFIG is not availabled", clusterName)
+
+		return clusterKind
+	}
+
+	nodes, err := nodes.List(cluster)
+	Expect(err).ToNot(HaveOccurred(), "Failed to get nodes list")
+
+	for _, node := range nodes {
+		nodeName := node.Definition.Name
+
+		_, isControlPlane := node.Object.Labels[RANConfig.ControlPlaneLabel]
+		_, isWorker := node.Object.Labels[RANConfig.WorkerLabel]
+
+		Expect(isWorker || isControlPlane).To(BeTrue(), "Node %s has neither control-plane nor worker label?", nodeName)
+
+		// node can be both control-plane and worker, so check each separately
+		if isControlPlane {
+			controlPlaneCount++
+		}
+
+		if isWorker {
+			workerCount++
+		}
+	}
+
+	glog.V(tsparams.LogLevel).Infof(
+		"controlPlaneCount: %d -- workerCount: %d", controlPlaneCount, workerCount)
+
+	switch {
+	case (controlPlaneCount == 3) && (workerCount == 2):
+		clusterKind = tsparams.ClusterStandard
+	case (controlPlaneCount == 3) && (workerCount == 3):
+		clusterKind = tsparams.ClusterThreeNode
+	case (controlPlaneCount == 1) && (workerCount == 2):
+		clusterKind = tsparams.ClusterSNOPlusWorker
+	case (controlPlaneCount == 1) && (workerCount == 1):
+		clusterKind = tsparams.ClusterSNO
+	}
+
+	return clusterKind
+}
+
+// getDeploymentMethod determines the deployment type as one of: SiteConfig with AgentClusterInstall,
+// ClusterInstance with AgentClusterInstall, or ClusterInstance with ImageClusterInstall.
+func getDeploymentMethod(
+	hub *clients.Settings,
+	clusterName string,
+	deploymentMethod tsparams.DeploymentType) tsparams.DeploymentType {
+	var (
+		deployment tsparams.DeploymentType = ""
+	)
+
+	if deploymentMethod == tsparams.DeploymentSiteConfig {
+		return deploymentMethod
+	}
+
+	clusterDeployment, err := hive.PullClusterDeployment(hub, clusterName, clusterName)
+
+	Expect(err).ToNot(HaveOccurred(), "Failed to get ClusterDeployment for cluster %s", clusterName)
+
+	Expect(hive.PullClusterDeployment(
+		hub, clusterName, clusterName)).ToNot(BeNil(), "ClusterDeployment for cluster %s is nil", clusterName)
+
+	installKind := clusterDeployment.Object.Spec.ClusterInstallRef.Kind
+	Expect(installKind).ToNot(BeEmpty(),
+		fmt.Sprintf("clusterdeployment %s does not have ClusterInstallRef.Kind value",
+			clusterName))
+
+	switch installKind {
+	case "ImageClusterInstall":
+		deployment = tsparams.DeploymentImageBasedCI
+	case "AgentClusterInstall":
+		deployment = tsparams.DeploymentAssistedCI
+	}
+
+	return deployment
+}
---
+++ tests/cnf/ran-deployment/internal/ranconfig/config.go
@@ -0,0 +1,187 @@
+package ranconfig
+
+import (
+	"github.com/golang/glog"
+	"github.com/kelseyhightower/envconfig"
+	"github.com/openshift-kni/eco-goinfra/pkg/clients"
+	"github.com/openshift-kni/eco-goinfra/pkg/ocm"
+	"github.com/openshift-kni/eco-gotests/tests/cnf/internal/cnfconfig"
+	"github.com/openshift-kni/eco-gotests/tests/cnf/ran-deployment/internal/ranparam"
+	"github.com/openshift-kni/eco-gotests/tests/cnf/ran-deployment/internal/version"
+)
+
+// RANConfig contains configuration for the RAN directory.
+type RANConfig struct {
+	*cnfconfig.CNFConfig
+	*HubConfig
+	*Spoke1Config
+	*Spoke2Config
+}
+
+// HubConfig contains the configuration for the hub cluster, if present.
+type HubConfig struct {
+	HubAPIClient  *clients.Settings
+	HubOCPVersion string
+	HubKubeconfig string `envconfig:"ECO_CNF_RAN_KUBECONFIG_HUB"`
+}
+
+// Spoke1Config contains the configuration for the spoke 1 cluster, which should always be present.
+type Spoke1Config struct {
+	Spoke1APIClient  *clients.Settings
+	Spoke1OCPVersion string
+	Spoke1Name       string `envconfig:"ECO_CNF_RAN_SPOKE1_NAME"`
+	Spoke1Kubeconfig string `envconfig:"KUBECONFIG"`
+}
+
+// Spoke2Config contains the configuration for the spoke 2 cluster, if present.
+type Spoke2Config struct {
+	Spoke2APIClient  *clients.Settings
+	Spoke2OCPVersion string
+	Spoke2Name       string
+	Spoke2Kubeconfig string `envconfig:"ECO_CNF_RAN_KUBECONFIG_SPOKE2"`
+}
+
+// NewRANConfig returns an instance of RANConfig.
+func NewRANConfig() *RANConfig {
+	glog.V(ranparam.LogLevel).Infof("Creating new RANConfig struct")
+
+	var ranConfig RANConfig
+	ranConfig.HubConfig = new(HubConfig)
+	ranConfig.Spoke1Config = new(Spoke1Config)
+	ranConfig.Spoke2Config = new(Spoke2Config)
+	ranConfig.CNFConfig = cnfconfig.NewCNFConfig()
+
+	err := readEnv(&ranConfig)
+	if err != nil {
+		glog.V(ranparam.LogLevel).Infof("Error reading main RAN Environment: %v", err)
+
+		return nil
+	}
+
+	ranConfig.newHubConfig()
+	ranConfig.newSpoke1Config()
+	ranConfig.newSpoke2Config()
+
+	return &ranConfig
+}
+
+func (ranconfig *RANConfig) newHubConfig() {
+	var err error
+
+	if ranconfig.HubConfig.HubKubeconfig == "" {
+		glog.V(ranparam.LogLevel).Info("No kubeconfig found for hub")
+
+		return
+	}
+
+	ranconfig.HubConfig.HubAPIClient = clients.New(ranconfig.HubConfig.HubKubeconfig)
+
+	ranconfig.HubConfig.HubOCPVersion, err = version.GetOCPVersion(ranconfig.HubConfig.HubAPIClient)
+	if err != nil {
+		glog.V(ranparam.LogLevel).Infof("Failed to get OCP version from hub: %v", err)
+
+		return
+	}
+
+	glog.V(ranparam.LogLevel).Infof("Found OCP version on hub: %s", ranconfig.HubConfig.HubOCPVersion)
+}
+
+func (ranconfig *RANConfig) newSpoke1Config() {
+	var err error
+
+	if ranconfig.Spoke1Config.Spoke1Kubeconfig == "" {
+		glog.V(ranparam.LogLevel).Infof("No spoke 1 kubeconfig specified in KUBECONFIG environment variable")
+
+		return
+	}
+
+	ranconfig.Spoke1Config.Spoke1APIClient = clients.New(ranconfig.Spoke1Config.Spoke1Kubeconfig)
+
+	if ranconfig.Spoke1APIClient == nil || ranconfig.Spoke1APIClient.KubeconfigPath == "" {
+		glog.V(ranparam.LogLevel).Infof("No spoke 1 API Client or KUBECONFIG defined")
+
+		return
+	}
+
+	klusterlet, err := ocm.PullKlusterlet(ranconfig.Spoke1Config.Spoke1APIClient, ocm.KlusterletName)
+	if err != nil {
+		glog.V(ranparam.LogLevel).Infof(
+			"Failed to get spoke 1 klusterlet at %s: %v", ranconfig.Spoke1Config.Spoke1Kubeconfig, err)
+
+		return
+	}
+
+	ranconfig.Spoke1Config.Spoke1Name = klusterlet.Object.Spec.ClusterName
+	if ranconfig.Spoke1Config.Spoke1Name == "" {
+		glog.V(ranparam.LogLevel).Infof(
+			"Failed to get spoke 1 name from klusterlet at %s: %v", ranconfig.Spoke1Config.Spoke1Kubeconfig, err)
+
+		return
+	}
+
+	glog.V(ranparam.LogLevel).Infof("Found cluster name on spoke 1: %s", ranconfig.Spoke1Config.Spoke1Name)
+
+	ranconfig.Spoke1Config.Spoke1OCPVersion, err = version.GetOCPVersion(ranconfig.Spoke1Config.Spoke1APIClient)
+	if err != nil {
+		glog.V(ranparam.LogLevel).Infof("Failed to get OCP version from spoke 1: %v", err)
+
+		return
+	}
+
+	glog.V(ranparam.LogLevel).Infof("Found OCP version on spoke 1: %s", ranconfig.Spoke1Config.Spoke1OCPVersion)
+}
+
+func (ranconfig *RANConfig) newSpoke2Config() {
+	var err error
+
+	if ranconfig.Spoke2Config.Spoke2Kubeconfig == "" {
+		glog.V(ranparam.LogLevel).Infof(
+			"No spoke 2 kubeconfig specified in ECO_CNF_RAN_KUBECONFIG_SPOKE2 environment variable")
+
+		return
+	}
+
+	ranconfig.Spoke2Config.Spoke2APIClient = clients.New(ranconfig.Spoke2Config.Spoke2Kubeconfig)
+
+	if ranconfig.Spoke2APIClient == nil || ranconfig.Spoke2APIClient.KubeconfigPath == "" {
+		glog.V(ranparam.LogLevel).Infof(
+			"No spoke 2 API Client, ECO_CNF_RAN_KUBECONFIG_SPOKE2 not defined, or spoke 2 KUBECONFIG file missing")
+
+		return
+	}
+
+	klusterlet, err := ocm.PullKlusterlet(ranconfig.Spoke2Config.Spoke2APIClient, ocm.KlusterletName)
+	if err != nil {
+		glog.V(ranparam.LogLevel).Infof(
+			"Failed to get spoke 2 klusterlet at %s: %v", ranconfig.Spoke2Config.Spoke2Kubeconfig, err)
+
+		return
+	}
+
+	ranconfig.Spoke2Config.Spoke2Name = klusterlet.Object.Spec.ClusterName
+	if ranconfig.Spoke2Config.Spoke2Name == "" {
+		glog.V(ranparam.LogLevel).Infof(
+			"Failed to get spoke 2 name from klusterlet at %s: %v", ranconfig.Spoke2Config.Spoke2Kubeconfig, err)
+
+		return
+	}
+
+	glog.V(ranparam.LogLevel).Infof("Found cluster name on spoke 2: %s", ranconfig.Spoke2Config.Spoke2Name)
+
+	ranconfig.Spoke2Config.Spoke2OCPVersion, err = version.GetOCPVersion(ranconfig.Spoke2Config.Spoke2APIClient)
+	if err != nil {
+		glog.V(ranparam.LogLevel).Infof("Failed to get OCP version from spoke 2: %v", err)
+
+		return
+	}
+
+	glog.V(ranparam.LogLevel).Infof("Found OCP version on spoke 2: %s", ranconfig.Spoke2Config.Spoke2OCPVersion)
+}
+
+func readEnv[C any](config *C) error {
+	var err error
+
+	envconfig.MustProcess("", config)
+
+	return err
+}
