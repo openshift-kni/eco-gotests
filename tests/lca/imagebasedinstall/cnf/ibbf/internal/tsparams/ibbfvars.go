c289e90893ed32ca8265ee566959f08dea068d49
Joshua Clark <josclark@.redhat.com>
2025-05-12 12:20:52 -0400

    Add IBBF Test POC

---
+++ tests/lca/imagebasedinstall/cnf/ibbf/internal/tsparams/ibbfvars.go
@@ -0,0 +1,55 @@
+package tsparams
+
+import (
+	lcav1 "github.com/openshift-kni/lifecycle-agent/api/imagebasedupgrade/v1"
+	configv1 "github.com/openshift/api/config/v1"
+	appsv1 "k8s.io/api/apps/v1"
+	batchv1 "k8s.io/api/batch/v1"
+	corev1 "k8s.io/api/core/v1"
+
+	"github.com/openshift-kni/eco-gotests/tests/lca/imagebasedinstall/cnf/internal/cnfparams"
+	"github.com/openshift-kni/k8sreporter"
+)
+
+const (
+	// LCANamespace is the namespace used by the lifecycle-agent.
+	LCANamespace = "openshift-lifecycle-agent"
+
+	// LCAWorkloadName is the name used for creating resources needed to backup workload app.
+	LCAWorkloadName = "ibu-workload-app"
+
+	// LCAOADPNamespace is the namespace used by the OADP operator.
+	LCAOADPNamespace = "openshift-adp"
+
+	// LCAKlusterletNamespace is the namespace that contains the klusterlet.
+	LCAKlusterletNamespace = "open-cluster-management-agent"
+)
+
+var (
+	// Labels represents the range of labels that can be used for test cases selection.
+	Labels = append(cnfparams.Labels, LabelSuite)
+
+	// ReporterNamespacesToDump tells to the reporter from where to collect logs.
+	ReporterNamespacesToDump = map[string]string{
+		LCANamespace:           "lca",
+		LCAWorkloadName:        "workload",
+		LCAKlusterletNamespace: "klusterlet",
+	}
+
+	// ReporterCRDsToDump tells to the reporter what CRs to dump.
+	ReporterCRDsToDump = []k8sreporter.CRData{
+		{Cr: &corev1.PodList{}},
+		{Cr: &batchv1.JobList{}},
+		{Cr: &corev1.ConfigMapList{}},
+		{Cr: &appsv1.DeploymentList{}},
+		{Cr: &corev1.ServiceList{}},
+		{Cr: &lcav1.ImageBasedUpgradeList{}},
+		{Cr: &configv1.ClusterOperatorList{}},
+	}
+
+	// TargetSnoClusterName is the name of target sno cluster.
+	TargetSnoClusterName string
+
+	// ClusterLabelSelector is the cluster label passed to IBGUs.
+	ClusterLabelSelector = map[string]string{"common": "true"}
+)
---
+++ tests/lca/imagebasedinstall/cnf/internal/cnfinittools/cnfinittools.go
@@ -0,0 +1,29 @@
+package cnfinittools
+
+import (
+	"github.com/openshift-kni/eco-goinfra/pkg/clients"
+	"github.com/openshift-kni/eco-gotests/tests/lca/imagebasedinstall/cnf/internal/cnfconfig"
+)
+
+var (
+	// TargetHubAPIClient is the api client to the target hub cluster.
+	TargetHubAPIClient *clients.Settings
+	// TargetSNOAPIClient is the api client to the target sno cluster.
+	TargetSNOAPIClient *clients.Settings
+	// CNFConfig provides access to general configuration parameters.
+	CNFConfig *cnfconfig.CNFConfig
+)
+
+// init loads all variables automatically when this package is imported. Once package is imported a user has full
+// access to all vars within init function. It is recommended to import this package using dot import.
+func init() {
+	CNFConfig = cnfconfig.NewCNFConfig()
+
+	if CNFConfig.TargetHubKubeConfig != "" {
+		TargetHubAPIClient = clients.New(CNFConfig.TargetHubKubeConfig)
+	}
+
+	if CNFConfig.TargetSNOKubeConfig != "" {
+		TargetSNOAPIClient = clients.New(CNFConfig.TargetSNOKubeConfig)
+	}
+}
