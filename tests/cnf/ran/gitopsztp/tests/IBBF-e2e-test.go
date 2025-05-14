89c4acd87078dca4ded33731a69ab9020cdce1b7
Joshua Clark <josclark@.redhat.com>
2025-05-14 13:24:24 -0400

    Refacrtored packges

---
+++ tests/cnf/ran/gitopsztp/tests/IBBF-e2e-test.go
@@ -0,0 +1,152 @@
+package tests
+
+import (
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"github.com/openshift-kni/eco-goinfra/pkg/configmap"
+	"github.com/openshift-kni/eco-goinfra/pkg/hive"
+	"github.com/openshift-kni/eco-goinfra/pkg/reportxml"
+	siteconfigv1alpha1 "github.com/openshift-kni/eco-goinfra/pkg/schemes/siteconfig/v1alpha1"
+	"github.com/openshift-kni/eco-goinfra/pkg/siteconfig"
+	"github.com/openshift-kni/eco-gotests/tests/cnf/ran/gitopsztp/internal/gitdetails"
+	"github.com/openshift-kni/eco-gotests/tests/cnf/ran/gitopsztp/internal/tsparams"
+	. "github.com/openshift-kni/eco-gotests/tests/cnf/ran/internal/raninittools"
+	"github.com/openshift-kni/eco-gotests/tests/cnf/ran/internal/ranparam"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	"fmt"
+	"time"
+)
+
+var _ = Describe(
+	"Performing Image-Based Break/Fix Flow",
+	Label(tsparams.LabelIBBFe2e), func() {
+
+		var (
+			spokeNamespace = RANConfig.Spoke1Name
+		)
+
+		AfterEach(func() {
+
+			By("Cleanup test configmap")
+
+			err := configmap.NewBuilder(HubAPIClient, tsparams.TestCMName, spokeNamespace).
+				Delete()
+			Expect(err).ToNot(HaveOccurred(), "Unable to delete configmap")
+		})
+
+		It("tests HW replacement via IBBF", reportxml.ID("78333"), func() {
+
+			gitDetails := tsparams.ArgoCdAppDetails[tsparams.ArgoCdClustersAppName]
+			testGitPath := gitdetails.JoinGitPaths([]string{
+				gitDetails.Path,
+				tsparams.ZtpTestPathIBBFe2e,
+			})
+
+			By("Checking if the git path exists")
+			if !gitdetails.DoesGitPathExist(gitDetails.Repo, gitDetails.Branch, testGitPath+tsparams.ZtpKustomizationPath) {
+				Skip(fmt.Sprintf("git path '%s' could not be found", testGitPath))
+			}
+
+			By("Enabling cluster reinstallation in SiteconfigOperator")
+
+			scoConfig, err := configmap.Pull(HubAPIClient, "siteconfig-operator-configuration",
+				ranparam.AcmOperatorNamespace)
+			Expect(err).ToNot(HaveOccurred(), "error pulling siteconfig-operator-configuration configmap")
+
+			scoConfig.Definition.Data["allowReinstalls"] = "true"
+			_, err = scoConfig.Update()
+			Expect(err).ToNot(HaveOccurred(), "error updating siteconfig-operator-configuration configmap to allow reinstalls")
+
+			By("Creating test configmap to be preserved")
+			configMap := configmap.NewBuilder(
+				HubAPIClient, tsparams.TestCMName, spokeNamespace).
+				WithData(map[string]string{"testValue": "true"})
+
+			// Apply preservation label to configmap
+			configMap.Definition.ObjectMeta.SetLabels(map[string]string{"siteconfig.open-cluster-management.io/preserve": ""})
+
+			// Create configmap
+			_, err = configMap.Create()
+			Expect(err).ToNot(HaveOccurred(), "error creating configmap")
+
+			// Get timestamp of configmap
+			configMapTimeStamp := configMap.Object.CreationTimestamp
+
+			By("Getting cluster identity pre-IBBF")
+			clusterDeployment, err := hive.PullClusterDeployment(HubAPIClient,
+				RANConfig.Spoke1Name,
+				spokeNamespace)
+			Expect(err).ToNot(HaveOccurred(), "error pulling clusterdeployment")
+
+			originalClusterID := clusterDeployment.Object.Spec.ClusterMetadata.ClusterID
+			originalInfraID := clusterDeployment.Object.Spec.ClusterMetadata.InfraID
+			originalUID := clusterDeployment.Object.ObjectMeta.UID
+
+			// Get clusterinstance.
+			clusterInstance, err := siteconfig.PullClusterInstance(
+				HubAPIClient, RANConfig.Spoke1Name, spokeNamespace)
+			Expect(err).ToNot(HaveOccurred(), "error pulling clusterinstance")
+
+			By("Changing policies app to point to IBBF test target directory")
+
+			gitDetails.Path = testGitPath
+
+			err = gitdetails.SetGitDetailsInArgoCd(
+				tsparams.ArgoCdClustersAppName,
+				gitDetails,
+				true,
+				true)
+			Expect(err).ToNot(HaveOccurred(), "Failed to update ArgoCD with new get details")
+
+			By("Waiting for clusterinstance re-installation to trigger")
+
+			_, err = clusterInstance.WaitForReinstallCondition(metav1.Condition{
+				Type:   string(siteconfigv1alpha1.ReinstallRequestProcessed),
+				Reason: string(siteconfigv1alpha1.Completed)}, 10*time.Minute)
+
+			Expect(err).ToNot(HaveOccurred(), "error waiting for clusterinstance to begin re-install")
+
+			By("Waiting for clusterinstance to start provisioning")
+
+			_, err = clusterInstance.WaitForReinstallCondition(metav1.Condition{
+				Type:   string(siteconfigv1alpha1.ClusterProvisioned),
+				Reason: string(siteconfigv1alpha1.InProgress)}, 5*time.Minute)
+
+			Expect(err).ToNot(HaveOccurred(), "error waiting for clusterinstance to begin provisioning")
+
+			By("Waiting for clusterinstance to finish provisioning")
+
+			_, err = clusterInstance.WaitForReinstallCondition(metav1.Condition{
+				Type:   string(siteconfigv1alpha1.ClusterProvisioned),
+				Reason: string(siteconfigv1alpha1.Completed)}, 30*time.Minute)
+
+			Expect(err).ToNot(HaveOccurred(), "error waiting for clusterinstance to complete re-install")
+
+			By("Verifying test configmap was preserved post-IBBF")
+
+			configMapPostIBBF, err := configmap.Pull(HubAPIClient,
+				tsparams.TestCMName, spokeNamespace)
+			Expect(err).ToNot(HaveOccurred(), "Preserved configmap is missing after IBBF")
+
+			Expect(configMapTimeStamp).ToNot(Equal(configMapPostIBBF.Object.CreationTimestamp),
+				"error: preserved configmap has the original timestamp")
+
+			By("Comparing cluster identity post-IBBF")
+			clusterDeploymentPostIBBF, err := hive.PullClusterDeployment(HubAPIClient,
+				RANConfig.Spoke1Name,
+				spokeNamespace)
+			Expect(err).ToNot(HaveOccurred(), "error pulling clusterdeployment")
+
+			Expect(originalClusterID).To(Equal(clusterDeploymentPostIBBF.Object.Spec.ClusterMetadata.ClusterID),
+				"error: reinstalled cluster has different ClusterID than original cluster")
+
+			Expect(originalInfraID).To(Equal(clusterDeploymentPostIBBF.Object.Spec.ClusterMetadata.InfraID),
+				"error: reinstalled cluster has different ClusterID than original cluster")
+
+			Expect(originalUID).To(Equal(string(clusterDeploymentPostIBBF.Object.ObjectMeta.UID)),
+				"error: reinstalled cluster has different ClusterID than original cluster")
+
+		})
+
+	})
--- tests/lca/imagebasedinstall/cnf/internal/cnfparams/const.go
+++
@@ -1,8 +0,0 @@
-package cnfparams
-
-const (
-	// CNFLogLevel custom loglevel for the cnf testing verbose mode.
-	CNFLogLevel = 50
-	// Label represents cnf label that can be used for test cases selection.
-	Label = "cnf"
-)
