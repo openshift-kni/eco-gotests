0a8b191a6fc6a70ff925f3cee0ce934ea82996b6
Kirsten Laskoski <klaskosk@redhat.com>
2025-07-07 16:57:13 -0400

    cnf ran: fix IBBF labels issue
    The default is to match all labels in a label filter, so the logic to not run the suite-level BeforeEach for IBBF tests was wrong. This commit fixes it with an equally temporary solution.

--- tests/cnf/ran/gitopsztp/ztp_suite_test.go
+++ tests/cnf/ran/gitopsztp/ztp_suite_test.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"path"
 	"runtime"
+	"strings"
 	"testing"
 	"time"
 
@@ -37,15 +38,19 @@ var _ = BeforeSuite(func() {
 })
 
 var _ = BeforeEach(func() {
-	if !Label("ibbf-end-to-end").MatchesLabelFilter(GinkgoLabelFilter()) {
-		By("deleting and recreating test namespace to ensure blank state")
-		for _, client := range []*clients.Settings{HubAPIClient, Spoke1APIClient} {
-			err := namespace.NewBuilder(client, tsparams.TestNamespace).DeleteAndWait(5 * time.Minute)
-			Expect(err).ToNot(HaveOccurred(), "Failed to delete ZTP test namespace")
+	// If we are specifically selecting the IBBF e2e test, then we should not run the BeforeEach. The spoke will not
+	// be present and the creation and deletion of the namespace will fail.
+	if strings.Contains(GinkgoLabelFilter(), tsparams.LabelIBBFe2e) {
+		return
+	}
 
-			_, err = namespace.NewBuilder(client, tsparams.TestNamespace).Create()
-			Expect(err).ToNot(HaveOccurred(), "Failed to create ZTP test namespace")
-		}
+	By("deleting and recreating test namespace to ensure blank state")
+	for _, client := range []*clients.Settings{HubAPIClient, Spoke1APIClient} {
+		err := namespace.NewBuilder(client, tsparams.TestNamespace).DeleteAndWait(5 * time.Minute)
+		Expect(err).ToNot(HaveOccurred(), "Failed to delete ZTP test namespace")
+
+		_, err = namespace.NewBuilder(client, tsparams.TestNamespace).Create()
+		Expect(err).ToNot(HaveOccurred(), "Failed to create ZTP test namespace")
 	}
 })
 
