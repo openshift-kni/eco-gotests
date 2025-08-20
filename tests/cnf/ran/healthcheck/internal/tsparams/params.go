7909402ea886811e719d7aecb8a10312cc1e3625
bazem <bazem@redhat.com>
2025-08-20 15:26:21 +0300

    add new healthcheck eco-go-test

---
+++ tests/cnf/ran/healthcheck/internal/tsparams/params.go
@@ -0,0 +1,8 @@
+package tsparams
+
+// this label for the health check test .
+// This label will be used in the Jenkins job's TEST_TYPE parameter.
+const LabelHealthCheckTestCases = "healthcheck"
+
+// defines the verbosity level for glog.
+const LogLevel = 100
